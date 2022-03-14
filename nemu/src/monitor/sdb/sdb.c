d1115bbe9f3594acec0c87fbaf24878475e1d984
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-14 03:44:20 -0700

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:44:20 up 56 min,  1 user,  load average: 0.32, 0.23, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -4,7 +4,7 @@
 #include <readline/history.h>
 #include "sdb.h"
 
-#define CLOSE "\001\033[0m\002"                 // 关闭所有属性
+#define CLOSE "\001\033[0m"                 // 关闭所有属性
 #define BLOD  "\001\033[1m\002"                 // 强调、加粗、高亮
 #define BEGIN(x,y) "\001\033["#x";"#y"m\002"    // x: 背景，y: 前景
 
