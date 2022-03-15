85ddc451267c8f022368c9df322154415032c181
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-15 04:31:25 -0700

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:31:25 up 43 min,  1 user,  load average: 0.51, 0.40, 0.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -116,7 +116,6 @@ void sdb_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        printf("%d\n", NR_CMD);
         if (cmd_table[i].handler(args) < 0) { return; }
         break;
       }
