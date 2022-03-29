122db2b2824ed5af3545f95c6f3390be6f6709aa
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 14:45:11 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:45:11 up 22 min,  1 user,  load average: 1.81, 1.17, 0.87

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -118,7 +118,8 @@ static int cmd_x(char *args) {
 
 static int cmd_p(char *args) {
   //int i;
-  bool *success = 1;
+  bool *success;
+  *sucess = true;
   if (args == NULL) { 
     printf("Please enter the expression\n");
     return 0;
