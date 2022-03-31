aaa28e2eb9c19ea042da70d46a619549e268f17a
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 09:55:49 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:55:49 up 53 min,  1 user,  load average: 0.56, 0.44, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -23,11 +23,13 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
+  {"\\-", '-'},         // sub
   {"==", TK_EQ},        // equal
   {"\\*", '*'},
   {"[0-9]+", TK_INT},   // decimal number
   {"\\(", '('},
   {"\\)", ')'},
+  {"\\/", '/'},
 
 };
 
@@ -112,7 +114,11 @@ static bool make_token(char *e) {
             case '*':
               tokens[nr_token].type = rules[i].token_type;
               nr_token ++;
-              break; 
+              break;
+            case '-' :
+              tokens[nr_token].type = rules[i].token_type;
+              nr_token ++;
+              break;
           default: Log("error!");
         }
 
@@ -137,9 +143,170 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-
+  eval(0, nr_token);
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
 
   return 0;
+}
+
+uint32_t eval(int p, int q) {
+  if (p > q) {
+    /* Bad expression */
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+  }
+  else if (check_parentheses(p, q) == 1) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1);
+  }
+  else if (check_parentheses(p, q) == 0){
+    int op = operator_find(p,q);
+    val1 = eval(p, op - 1);
+    val2 = eval(op + 1, q);
+
+    switch (op_type) {
+      case '+': 
+        return val1 / val2;
+      case '-': 
+        return val1 / val2;
+      case '*': 
+        return val1 / val2;
+      case '/': 
+        return val1 / val2;
+      default: assert(0);
+    }
+  }
+  else  assert(0);
+}
+
+int check_parentheses(int p, int q)
+{
+  int i = 0;
+  int lparenthesis=0,rparenthesis=0,parenthesis = 0;
+  for(i = q; i<=q; i++)
+  {
+    if(tokens[i].type = '(')  parenthesis++;
+    else if(tokens[i].type = ')') parenthesis--;
+  }
+  if(parenthesis!= 0) return -1;  //fatal error: bad expression--parenthesis dose not match
+
+  int length = q-p+1;
+  int flag=0;
+  if(tokens[p].type=='(' && tokens[q].type==')'){
+    flag = 1;
+	}
+	int i,j;
+  i = fun(p, q);
+  j = fun(p+1, q-1);
+    
+    
+    if (i == 1) {
+    	if(flag == 1&&j== 1){
+			printf("匹配且被包围");
+      return 1; 	
+		}
+		else{
+			printf("匹配但不被包围,可以求值\n");
+      return 0;
+		}
+    }
+    else {
+        printf("括号不匹配！\n");
+        return -1;
+    }
+    return 0;
+}
+
+int fun(int start, int end)
+{
+    char chLeft;        // 左括号
+    char chRight;       // 右括号
+    while ((start <= end) && (str[start] != '\0'))
+    {
+        switch (tokens[start].type)
+        {
+        case '(':
+            chLeft = tokens[start].type;
+            chRight = ')';
+            break;
+        case ')':
+            return 0;
+        default:
+            chLeft = '\0';
+            break;
+        }
+        if (tokens[start].type == chLeft)
+        {
+            int a = 1;
+            int b = 0;
+            int t = start + 1;
+            while ((t <= end) && (str[t] != '\0')) // 搜索匹配的右括号 
+            {
+                if (tokens[t].type == chLeft)
+                    ++a;
+                if (tokens[t].type == chRight)
+                    ++b;
+                if (b>a)
+                    return 0;
+                if (a == b)      // 再对匹配括号里面的括号进行匹配 
+                {
+                    if (0 == fun(start + 1, t - 1)) // 递归调用 
+                        return 0;
+                    start = t;
+                    break;
+                }
+                ++t;
+            }
+            if (a>b)
+                return 0;
+        }
+        ++start;
+    }
+    return 1;
+}
+
+int operator_find(p,q){
+  int i,nr_p=0;
+  int op,pir = -1;
+  for (i = p,i <= 1; i++){
+    if (tokens[t].type == '('){
+      nr_p++;
+      i++;
+
+      while (i <= q) // 搜索匹配的右括号 
+      {
+        if (tokens[t].type == '(')
+            ++nr_p;
+        if (tokens[t].type == ')')
+            --nr_p;
+        i++;    
+        if (nr_p == 0){
+          break;
+        }
+      }
+    }
+    if (tokens[t].type == '+' ||tokens[t].type == '-' ||tokens[t].type == '*' ||tokens[t].type == '/'){
+      if (op(tokens[t].type)>pir) {
+        op = i;
+      }   
+    }
+  }
+  return op;
+}
+int pir(char op)
+{
+  switch (op)
+  {
+  case '*':
+  case '/': return 0;
+  case '+':
+  case '-': return 1;
+  }
 }
