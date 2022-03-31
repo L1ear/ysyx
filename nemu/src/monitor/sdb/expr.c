11c80d4c6b6948ffd042aace3ea4b13db0eebfc9
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 14:36:45 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:36:45 up 33 min,  1 user,  load average: 0.17, 0.22, 0.25

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -299,8 +299,9 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  eval(0, nr_token);
   printf("%d\n",nr_token);
+  eval(0, nr_token);
+  
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
 
