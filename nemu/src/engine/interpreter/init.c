baeda80350f37e6e71a2656566ea76c1e274116d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-01 22:55:50 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:55:50 up 47 min,  1 user,  load average: 0.25, 0.25, 0.29

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -8,24 +8,24 @@ void engine_start() {
   cpu_exec(-1);
 #else
   /* Receive commands from user. */
-  // sdb_mainloop();
+  sdb_mainloop();
   /* Test expr */
-  FILE *file = fopen("/home/qw/ysyx-workbench/nemu/tools/gen-expr/input", "r");     
-  if(file == NULL)
-  {
-      printf("open error!\n");
-  }
-  else printf("open successful!\n");
-  int i;
-  for (i = 0;i<100; i++) {
-    char buf[1024] = {0};
-    word_t result = 0;
-    static bool *success;
-    assert(fscanf(file, "%lu %s", &result, buf));
-     printf("%lu\n", result);
-     printf("%lu\n", expr(buf, success));
-    assert((result == expr(buf, success)));
-  }
+  // FILE *file = fopen("/home/qw/ysyx-workbench/nemu/tools/gen-expr/input", "r");     
+  // if(file == NULL)
+  // {
+  //     printf("open error!\n");
+  // }
+  // else printf("open successful!\n");
+  // int i;
+  // for (i = 0;i<100; i++) {
+  //   char buf[1024] = {0};
+  //   word_t result = 0;
+  //   static bool *success;
+  //   assert(fscanf(file, "%lu %s", &result, buf));
+  //    printf("%lu\n", result);
+  //    printf("%lu\n", expr(buf, success));
+  //   assert((result == expr(buf, success)));
+  // }
   
   
 #endif
