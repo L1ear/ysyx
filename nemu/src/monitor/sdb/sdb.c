80d361feaccdbc8937bcddacf7ff5137160a8b51
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-01 23:12:22 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:12:22 up  1:03,  1 user,  load average: 0.53, 0.41, 0.35

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -119,16 +119,15 @@ static int cmd_x(char *args) {
 static int cmd_p(char *args) {
   //int i;
   word_t value=0;
-  int suc =0;
-  static bool *success = &suc;
+  static bool success;
   if (args == NULL) { 
     Log("Please enter the expression\n");
     return 0;
   }
-  value = expr(args, success);
-  printf("???%d\n",*success);
+  value = expr(args, &success);
+  printf("???%d\n",success);
   Log("test3\n");
-  if(*success == false){
+  if(success == false){
     Log("please enter a solvable expression\n");
     return 0;
   }
