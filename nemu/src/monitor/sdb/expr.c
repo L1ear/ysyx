27455b130e935e1970320691261f727795e73a96
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-01 23:02:33 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:02:33 up 54 min,  1 user,  load average: 0.28, 0.27, 0.27

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -169,9 +169,10 @@ word_t eval(int p, int q) {
     
     uint32_t val1 = eval(p, op - 1);
     uint32_t val2 = eval(op + 1, q);
-Log("op: %d\n",op);
+
     switch (tokens[op].type) {
       case '+':
+        Log("op: %d\n",op);
         return val1 + val2;
       case '-': 
         return val1 - val2;
