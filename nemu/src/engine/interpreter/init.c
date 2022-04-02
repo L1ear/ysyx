87a8972fb42166698db9d80dc4dda77fbc7fbf66
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 23:12:23 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:12:23 up  1:04,  1 user,  load average: 0.37, 0.17, 0.34

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -8,33 +8,33 @@ void engine_start() {
   cpu_exec(-1);
 #else
   /* Receive commands from user. */
-  // sdb_mainloop();
+  sdb_mainloop();
   /* Test expr */
-  FILE *file = fopen("/home/qw/ysyx-workbench/nemu/tools/gen-expr/input", "r");     
-  if(file == NULL)
-  {
-      printf("open error!\n");
-  }
-  else printf("open successful!\n");
-  int i;
-  int err[10000];
-  int cnt = 0;
-  for (i = 0;i<5000; i++) {
-    char buf[1024] = {0};
-    word_t result = 0;
-    static bool success;
-    assert(fscanf(file, "%lu %s", &result, buf));
-    //  printf("%lu %s\n", result,buf);
-     word_t expr_value = expr(buf, &success);
-    //  printf("expr_value: %lu\n", expr_value);
+  // FILE *file = fopen("/home/qw/ysyx-workbench/nemu/tools/gen-expr/input", "r");     
+  // if(file == NULL)
+  // {
+  //     printf("open error!\n");
+  // }
+  // else printf("open successful!\n");
+  // int i;
+  // int err[10000];
+  // int cnt = 0;
+  // for (i = 0;i<5000; i++) {
+  //   char buf[1024] = {0};
+  //   word_t result = 0;
+  //   static bool success;
+  //   assert(fscanf(file, "%lu %s", &result, buf));
+  //   //  printf("%lu %s\n", result,buf);
+  //    word_t expr_value = expr(buf, &success);
+  //   //  printf("expr_value: %lu\n", expr_value);
      
-     if(result != expr_value){
-       err[cnt] = i+1;
-       cnt++;
-     }
-  }
+  //    if(result != expr_value){
+  //      err[cnt] = i+1;
+  //      cnt++;
+  //    }
+  // }
   
-  for (i = 0;i<cnt;i++) printf("neg: %d\n", err[i]);
-  Log("Successed!");
+  // for (i = 0;i<cnt;i++) printf("neg: %d\n", err[i]);
+  // Log("Successed!");
 #endif
 }
