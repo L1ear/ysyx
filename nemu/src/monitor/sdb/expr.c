3daeab1f4d1361d16e5161a63008a11ae954820b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 11:16:46 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:16:46 up 21 min,  1 user,  load average: 0.50, 0.39, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -132,7 +132,7 @@ static bool make_token(char *e) {
             case TK_HEX :
               tokens[nr_token].type = rules[i].token_type;
               strncpy(tokens[nr_token].str, substr_start+2, substr_len);
-              Log("get number: %d\n",htoi(tokens[nr_token].str));
+              Log("get number: %ld\n",htoi(tokens[nr_token].str));
               nr_token ++;
               break;
           default: Log("error!");
