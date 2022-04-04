12c362993bdda7d78347c771186d40e3a882847c
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 11:09:32 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:09:32 up 14 min,  1 user,  load average: 0.51, 0.34, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -31,7 +31,7 @@ static struct rule {
   {"\\(", '('},
   {"\\)", ')'},
   {"\\/", '/'},
-  {"/^([1-9a-fA-F][\da-fA-F]*|0)$/",TK_HEX}  //hex number
+  {"^([1-9a-fA-F][\\da-fA-F]*|0)$",TK_HEX}  //hex number
 
 };
 
