ced4b42820c00f9b0c48574d644519bc2223869d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 16:08:20 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:08:20 up 44 min,  1 user,  load average: 0.13, 0.12, 0.09

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -74,12 +74,13 @@ void free_wp(int n){
     }
     
     Log("i:%d\n",i);
-    Log("(p->next):%lx\n",(word_t)((p->next)));
+    // Log("(p->next):%lx\n",(word_t)((p->next)));
     if(((p->next)->next) == NULL){
       Log("????????");
       q = p;         //提前预判要删除的是不是最后一个
     }
     if ((i+1) == n) p->next = (p->next)->next;
+    Log("(p->next):%lx\n",(word_t)((p->next)));
     p = x;
   }
   if (p != NULL && q != NULL){
