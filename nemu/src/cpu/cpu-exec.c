572ca0dae5a7b5cb4c983ced7d07de842c39a6f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 22:51:48 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:51:48 up 30 min,  1 user,  load average: 0.22, 0.20, 0.24

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -17,7 +17,7 @@ static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
 void device_update();
-extern struct watchpoint WP;
+
 extern WP *begin;
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
