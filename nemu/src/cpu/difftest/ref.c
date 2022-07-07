d36cae1f865cadc82ac9925e2760221645fe3be4
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 20:19:32 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:19:32 up  6:24,  1 user,  load average: 0.33, 0.42, 0.41

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -5,6 +5,7 @@
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
+    printf("%ld\n",n);
     uint8_t *mem = buf;
     int i;
     for(i = n;i>0;i--){
