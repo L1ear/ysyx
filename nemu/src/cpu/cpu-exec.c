72d4e41ee6cab7fa6fa971acdc77673209cd15a6
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-09 14:54:23 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:54:23 up  4:10,  1 user,  load average: 0.86, 0.56, 0.38

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -18,6 +18,11 @@ static bool g_print_step = false;
 
 void device_update();
 
+#ifdef CONFIG_ITRACE 
+  void add_iringbuf(char *newlog);
+  void print_iringbuf();
+#endif
+
 extern WP *begin;
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
@@ -68,6 +73,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+  add_iringbuf(s->logbuf);
 #endif
 }
 
@@ -122,7 +128,11 @@ void cpu_exec(uint64_t n) {
     break;
 
     case NEMU_END: printf("%d\n",nemu_state.state);
+#ifdef CONFIG_ITRACE
+  print_iringbuf();
+#endif    
     case NEMU_ABORT:
+
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ASNI_FMT("ABORT", ASNI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN) :
--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -17,3 +17,38 @@ bool log_enable() {
   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
          (g_nr_guest_inst <= CONFIG_TRACE_END), false);
 }
+
+
+typedef struct {
+  char* logbuf;
+}ringbuf;
+static ringbuf iringbuf[10];    //10个缓冲区
+static int nr_i = 0;
+
+void init_iringbuf(){
+  for(int i=0;i<nr_i;i++){
+    iringbuf[i].logbuf = NULL;
+  }
+}
+
+void add_iringbuf(char *newlog){
+  nr_i = nr_i % 10;
+  free(iringbuf[nr_i].logbuf);
+  iringbuf[nr_i].logbuf = (char *) malloc(sizeof(char)*(strlen(newlog)+1));
+  strcpy(iringbuf[nr_i].logbuf,newlog);
+  nr_i++;
+}
+
+void print_iringbuf(){
+  //第一个for循环打印 [index , MAX_BUF]
+  for(int i=nr_i; i<10; i++){
+      printf("%s\n", iringbuf[i].logbuf);
+      free(iringbuf[i].logbuf);
+  }
+  printf("\n");
+  //第二个for循环打印 [0 , index-1]
+  for(int i=0; i<nr_i; i++){ 
+      printf("%s\n", iringbuf[i].logbuf);
+      free(iringbuf[i].logbuf);
+  }
+}
