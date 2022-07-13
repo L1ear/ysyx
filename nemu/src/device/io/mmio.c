7c96bbaed662e9b868209bba852b65fa77fb298d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 22:00:14 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:00:14 up 42 min,  1 user,  load average: 0.46, 0.42, 0.40

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -25,6 +25,7 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 word_t mmio_read(paddr_t addr, int len) {
   IOMap* map = fetch_mmio_map(addr);
   #ifdef CONFIG_DTRACE
+  if(map->name != "rtc")
     Log("read device: %s\n",map->name);
   #endif
   
@@ -34,6 +35,7 @@ word_t mmio_read(paddr_t addr, int len) {
 void mmio_write(paddr_t addr, int len, word_t data) {
   IOMap* map = fetch_mmio_map(addr);
   // #ifdef CONFIG_DTRACE
+    if(map->name != ("rtc" || "serial"))
     Log("write "FMT_WORD" to device: %s\n", data, map->name);
   // #endif
   map_write(addr, len, data, map);
