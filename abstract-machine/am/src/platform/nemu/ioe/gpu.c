53fbcbce129393a523e4b83b2fa2483377ee5273
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 21:56:46 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:56:46 up 52 min,  1 user,  load average: 0.00, 0.16, 0.25

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,6 +23,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = (uint32_t*)(ctl->pixels);
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w, h = ctl->h;
+  for(int j = 0; j< h; j++){
+    for(int i = 0; i< w; i++){
+      fb[(y+j)*W+(x+i)] = *(pixels+j*w+i);
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
