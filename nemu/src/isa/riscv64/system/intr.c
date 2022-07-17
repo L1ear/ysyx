508fcee14fe7c60c9829c84c743d3d9fa4c4578d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-17 14:12:30 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:12:30 up  3:01,  1 user,  load average: 1.31, 0.57, 0.28

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -1,15 +1,13 @@
 #include <isa.h>
 
-#include <cpu/decode.h>
-
-void isa_raise_intr(word_t NO, vaddr_t epc, Decode *s) {
+word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
   cpu.mepc = epc;
   cpu.mcause = NO;
   Log("ecall has been called,mcause: %016lx\n",NO);
-  s->dnpc = cpu.mtvec;
+  return cpu.mtvec;
 }
 
 word_t isa_query_intr() {
