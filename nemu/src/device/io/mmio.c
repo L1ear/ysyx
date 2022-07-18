e9600dfff340ddc623dd668bbe1255f8c34fb306
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 19:01:48 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:01:48 up 54 min,  1 user,  load average: 0.65, 0.65, 0.52

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -27,7 +27,7 @@ word_t mmio_read(paddr_t addr, int len) {
   #ifdef CONFIG_DTRACE
   char namer[32];
   sscanf(map->name,"%s",namer);
-  if(strcmp(namer,"rtc")<0||strcmp(namer,"serial")==0)      //如果是rtc或串口，就只写入log不输出，防止挤爆终端，下同
+  if(strcmp(namer,"rtc")==0||strcmp(namer,"serial")==0)      //如果是rtc或串口，就只写入log不输出，防止挤爆终端，下同
     Log("read device: %s\n",namer);
   else
     log_write("read device: %s\n",namer);
