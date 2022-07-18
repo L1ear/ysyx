ca156f285a8fe43e94c27cd7a3b352c36cb2632e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 18:50:30 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:50:30 up 43 min,  1 user,  load average: 0.39, 0.45, 0.37

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -42,7 +42,7 @@ void mmio_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_DTRACE
   char name[32];
   sscanf(map->name,"%s",name);
-  if(strcmp(name,"rtc")&&strcmp(name,"serial") == 1)
+  if(strcmp(name,"vmem")&&strcmp(name,"serial") == 1)
     Log("write "FMT_WORD" to device: %s\n", data, map->name);
   else
     log_write("write "FMT_WORD" to device: %s\n", data, map->name);
