912a49ff1c1e8b9eaab5625069f7ad8f615d7782
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 08:58:49 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:58:49 up  1:34,  1 user,  load average: 1.10, 0.95, 0.89

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -63,19 +63,19 @@ void print_iringbuf(){
 #ifdef CONFIG_FTRACE
 void init_ftrace(const char *elf_file) {
   elf_fp = stdout;
-  int rtval;
+  // int rtval;
   if (elf_file != NULL) {
     FILE *fp = fopen(elf_file, "w");
     Assert(fp, "Can not open '%s'", elf_file);
     elf_fp = fp;
   }
-  rtval = fread(elf_head, sizeof(Elf64_Ehdr), 1, elf_fp);
-  int nr_sc = elf_head->e_shnum;
-  fseek(elf_fp,elf_head->e_shoff,SEEK_SET);
-  rtval = fread(shdr, sizeof(Elf64_Shdr),nr_sc, elf_fp);
-  printf("rtval:%d\n",rtval);
-  for(int i = 0; i<nr_sc; ++i){
-    printf("%d  %c\n",i,shdr[i].sh_name);
-  }
+  // rtval = fread(elf_head, sizeof(Elf64_Ehdr), 1, elf_fp);
+  // int nr_sc = elf_head->e_shnum;
+  // fseek(elf_fp,elf_head->e_shoff,SEEK_SET);
+  // rtval = fread(shdr, sizeof(Elf64_Shdr),nr_sc, elf_fp);
+  // printf("rtval:%d\n",rtval);
+  // for(int i = 0; i<nr_sc; ++i){
+  //   printf("%d  %c\n",i,shdr[i].sh_name);
+  // }
 }
 #endif
