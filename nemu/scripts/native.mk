ff2dd9529279d40285be73b52529542835b66248
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 16:32:19 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:19 up  1:39,  1 user,  load average: 0.52, 0.37, 0.25

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -15,7 +15,10 @@ override ARGS += $(ARGS_DIFF)
 # Command to execute NEMU
 IMG ?=
 ELF	?=
-NEMU_EXEC := $(BINARY) $(ARGS) --e $(ELF) $(IMG) 
+ifdef CONFIG_FTRACE
+ARGS += --e $(ELF)
+endif
+NEMU_EXEC := $(BINARY) $(ARGS) $(IMG) 
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
