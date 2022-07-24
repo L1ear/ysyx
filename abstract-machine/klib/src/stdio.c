6890738f647bfacdc81aaff0a7b868077468c2ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 19:22:08 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:22:08 up  3:07,  1 user,  load average: 0.16, 0.24, 0.23

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,162 +1,3 @@
-// #include <am.h>
-// #include <klib.h>
-// #include <klib-macros.h>
-// #include <stdarg.h>
-
-// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-// int printf(const char *fmt, ...) {
-//   // panic("Not implemented");
-//   char ans[2000]={0};
-//   va_list ap;
-//   int ret = 0;
-//   va_start(ap, fmt);
-//   ret = vsprintf(ans, fmt, ap);
-//   va_end(ap);
-//   for(int i = 0; ans[i] != '\0';i++)
-//   {
-//     putch(ans[i]);
-//   }
-//   return ret;
-// }
-
-// int vsprintf(char *out, const char *fmt, va_list ap) {
-//   // panic("Not implemented");
-//   size_t j = 0, k = 0 ,i = 0;
-//   int val, num[64] = {0};
-//   uint32_t v;
-//   const char* str;
-//   char nums[20];
-//   while (*fmt != '\0')
-//   {
-//     switch (*fmt)
-//     {
-//     case '%':
-//       //精度,中间还没留精度的选项
-//       fmt++;
-//       if (*fmt >= 'a' && *fmt <= 'z')
-//       {
-//         switch (*fmt)
-//         {
-//           case 'd':
-//             val = va_arg(ap, int);
-//             k = 0;
-//             if (val == 0x80000000)
-//             {
-//               out[j++] = '-';out[j++] = '2';out[j++] = '1';out[j++] = '4';out[j++] = '7';out[j++] = '4';
-//               out[j++] = '8';out[j++] = '3';out[j++] = '6';out[j++] = '4';out[j++] = '8';
-//               fmt++;
-//               break;
-//             }
-//             else if (val < 0)
-//             {
-//               val = (-1) * val;
-//               out[j++] = '-';
-//             }
-//             do
-//             {
-//               num[k++] = val % 10 + '0';
-//               val = val / 10;
-//             } while (val != 0);
-//             for (int ii = k - 1; ii >= 0; ii--)
-//             {
-//               out[j++] = num[ii];
-//             }
-//             fmt++;
-//             break;
-//           case 's':
-//             str = va_arg(ap, char *);
-//             i= 0;
-//             while(str[i] != '\0')
-//             {
-//               out[j++] = str[i++];
-//             }
-//             fmt++;
-//             break;  
-//           case 'x':  
-//             v = va_arg(ap,uint32_t);
-//             k = 0;
-//             out[j++]='0';
-//             out[j++]='x';
-//             if(v == 0){
-//                 out[j++] = '0';
-//                 fmt++;
-//                 break;
-//             }
-//             while(v != 0)
-//             {
-//               nums[k++] = v%16 < 10? v%16+'0':'a'+v%16-10;
-//               v = v/16;
-//             }
-//             for(int ii=k-1;ii>=0;ii--){
-//               out[j++]=nums[ii];
-//             }
-//             fmt++;
-//             break; 
-//           case 'p':
-//             v = va_arg(ap,uint32_t);
-//             k = 0;
-//             out[j++]='0';
-//             out[j++]='x';
-//             if(v == 0){
-//               out[j++] = '0';
-//               fmt++;
-//               break;
-//             }
-//             while(v != 0)
-//             {
-//               nums[k++] = v%16 < 10? v%16+'0':'a'+v%16-10;
-//               v = v/16;
-//             }
-//             for(int ii=k-1;ii>=0;ii--){
-//               out[j++]=nums[ii];
-//             }
-//             fmt++;
-//             break;
-//           case 'c':
-//             out[j++] = va_arg(ap,int);
-//             fmt++;
-//             break;  
-//           default:
-//             assert(0);
-//           //other fuctions remaining to be realized.
-//         }
-//       } //处理标志符及一个字母中间的数
-//       else{
-//         out[j++] = *fmt;
-//         fmt++;
-//       }
-//       break;
-//     default:
-//       out[j++] = *fmt;
-//       fmt++;
-//     }
-//   }
-//   out[j] = '\0';
-//   return j;
-// }
-
-// int sprintf(char *out, const char *fmt, ...) {
-//   // panic("Not implemented");
-//   va_list ap;
-//   int ret = 0;
-//   va_start(ap, fmt);
-//   ret = vsprintf(out, fmt, ap);
-//   va_end(ap);
-//   return ret;
-// }
-
-// int snprintf(char *out, size_t n, const char *fmt, ...) {
-//   panic("Not implemented");
-// }
-
-// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-//   panic("Not implemented");
-// }
-
-// #endif
-
-
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
@@ -164,269 +5,145 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define ZEROPAD	1		/* pad with zero */
-#define SIGN	2		/* unsigned/signed long */
-#define PLUS	4		/* show plus */
-#define SPACE	8		/* space if plus */
-#define LEFT	16		/* left justified */
-#define SMALL	32		/* Must be 32 == 0x20 */
-#define SPECIAL	64		/* 0x */
-
-#define __do_div(n, base) ({ \
-int __res; \
-__res = ((unsigned long) n) % (unsigned) base; \
-n = ((unsigned long) n) / (unsigned) base; \
-__res; })
-
-static inline int isdigit(int c)
-{
-	return '0' <= c && c <= '9';
+int printf(const char *fmt, ...) {
+  // panic("Not implemented");
+  char ans[2000]={0};
+  va_list ap;
+  int ret = 0;
+  va_start(ap, fmt);
+  ret = vsprintf(ans, fmt, ap);
+  va_end(ap);
+  for(int i = 0; ans[i] != '\0';i++)
+  {
+    putch(ans[i]);
+  }
+  return ret;
 }
 
-static int skip_atoi(const char **s)
-{
-	int i = 0;
-	while (isdigit(**s))
-		i = i * 10 + *((*s)++) - '0';
-	return i;
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  // panic("Not implemented");
+  size_t j = 0, k = 0 ,i = 0;
+  int val, num[64] = {0};
+  uint32_t v;
+  const char* str;
+  char nums[20];
+  while (*fmt != '\0')
+  {
+    switch (*fmt)
+    {
+    case '%':
+      //精度,中间还没留精度的选项
+      fmt++;
+      if (*fmt >= 'a' && *fmt <= 'z')
+      {
+        switch (*fmt)
+        {
+          case 'd':
+            val = va_arg(ap, int);
+            k = 0;
+            if (val == 0x80000000)
+            {
+              out[j++] = '-';out[j++] = '2';out[j++] = '1';out[j++] = '4';out[j++] = '7';out[j++] = '4';
+              out[j++] = '8';out[j++] = '3';out[j++] = '6';out[j++] = '4';out[j++] = '8';
+              fmt++;
+              break;
+            }
+            else if (val < 0)
+            {
+              val = (-1) * val;
+              out[j++] = '-';
+            }
+            do
+            {
+              num[k++] = val % 10 + '0';
+              val = val / 10;
+            } while (val != 0);
+            for (int ii = k - 1; ii >= 0; ii--)
+            {
+              out[j++] = num[ii];
+            }
+            fmt++;
+            break;
+          case 's':
+            str = va_arg(ap, char *);
+            i= 0;
+            while(str[i] != '\0')
+            {
+              out[j++] = str[i++];
+            }
+            fmt++;
+            break;  
+          case 'x':  
+            v = va_arg(ap,uint32_t);
+            k = 0;
+            out[j++]='0';
+            out[j++]='x';
+            if(v == 0){
+                out[j++] = '0';
+                fmt++;
+                break;
+            }
+            while(v != 0)
+            {
+              nums[k++] = v%16 < 10? v%16+'0':'a'+v%16-10;
+              v = v/16;
+            }
+            for(int ii=k-1;ii>=0;ii--){
+              out[j++]=nums[ii];
+            }
+            fmt++;
+            break; 
+          case 'p':
+            v = va_arg(ap,uint32_t);
+            k = 0;
+            out[j++]='0';
+            out[j++]='x';
+            if(v == 0){
+              out[j++] = '0';
+              fmt++;
+              break;
+            }
+            while(v != 0)
+            {
+              nums[k++] = v%16 < 10? v%16+'0':'a'+v%16-10;
+              v = v/16;
+            }
+            for(int ii=k-1;ii>=0;ii--){
+              out[j++]=nums[ii];
+            }
+            fmt++;
+            break;
+          case 'c':
+            out[j++] = va_arg(ap,int);
+            fmt++;
+            break;  
+          default:
+            assert(0);
+          //other fuctions remaining to be realized.
+        }
+      } //处理标志符及一个字母中间的数
+      else{
+        out[j++] = *fmt;
+        fmt++;
+      }
+      break;
+    default:
+      out[j++] = *fmt;
+      fmt++;
+    }
+  }
+  out[j] = '\0';
+  return j;
 }
 
-static char *number(char *str, long num, int base, int size, int precision,
-		    int type)
-{
-	/* we are called with base 8, 10 or 16, only, thus don't need "G..."  */
-	static const char digits[16] = "0123456789ABCDEF"; /* "GHIJKLMNOPQRSTUVWXYZ"; */
-	char tmp[66];
-	char c, sign, locase;
-	int i;
-	/* locase = 0 or 0x20. ORing digits or letters with 'locase'
-	 * produces same digits or (maybe lowercased) letters */
-	locase = (type & SMALL);
-	if (type & LEFT)
-		type &= ~ZEROPAD;
-	if (base < 2 || base > 16)
-		return NULL;
-	c = (type & ZEROPAD) ? '0' : ' ';
-	sign = 0;
-	if (type & SIGN) {
-		if (num < 0) {
-			sign = '-';
-			num = -num;
-			size--;
-		} else if (type & PLUS) {
-			sign = '+';
-			size--;
-		} else if (type & SPACE) {
-			sign = ' ';
-			size--;
-		}
-	}
-	if (type & SPECIAL) {
-		if (base == 16)
-			size -= 2;
-		else if (base == 8)
-			size--;
-	}
-	i = 0;
-	if (num == 0)
-		tmp[i++] = '0';
-	else
-		while (num != 0)
-			tmp[i++] = (digits[__do_div(num, base)] | locase);
-	if (i > precision)
-		precision = i;
-	size -= precision;
-	if (!(type & (ZEROPAD + LEFT)))
-		while (size-- > 0)
-			*str++ = ' ';
-	if (sign)
-		*str++ = sign;
-	if (type & SPECIAL) {
-		if (base == 8)
-			*str++ = '0';
-		else if (base == 16) {
-			*str++ = '0';
-			*str++ = ('X' | locase);
-		}
-	}
-	if (!(type & LEFT))
-		while (size-- > 0)
-			*str++ = c;
-	while (i < precision--)
-		*str++ = '0';
-	while (i-- > 0)
-		*str++ = tmp[i];
-	while (size-- > 0)
-		*str++ = ' ';
-	return str;
-}
-
-int vsprintf(char *buf, const char *fmt, va_list args)
-{
-	int len;
-	unsigned long num;
-	int i, base;
-	char *str;
-	const char *s;
-	int flags;		/* flags to number() */
-	int field_width;	/* width of output field */
-	int precision;		/* min. # of digits for integers; max
-				   number of chars for from string */
-	int qualifier;		/* 'h', 'l', or 'L' for integer fields */
-
-	for (str = buf; *fmt; ++fmt) {
-		if (*fmt != '%') {
-			*str++ = *fmt;
-			continue;
-		}
-
-		/* process flags */
-		flags = 0;
-	      repeat:
-		++fmt;		/* this also skips first '%' */
-		switch (*fmt) {
-		case '-':
-			flags |= LEFT;
-			goto repeat;
-		case '+':
-			flags |= PLUS;
-			goto repeat;
-		case ' ':
-			flags |= SPACE;
-			goto repeat;
-		case '#':
-			flags |= SPECIAL;
-			goto repeat;
-		case '0':
-			flags |= ZEROPAD;
-			goto repeat;
-		}
-
-		/* get field width */
-		field_width = -1;
-		if (isdigit(*fmt))
-			field_width = skip_atoi(&fmt);
-		else if (*fmt == '*') {
-			++fmt;
-			/* it's the next argument */
-			field_width = va_arg(args, int);
-			if (field_width < 0) {
-				field_width = -field_width;
-				flags |= LEFT;
-			}
-		}
-
-		/* get the precision */
-		precision = -1;
-		if (*fmt == '.') {
-			++fmt;
-			if (isdigit(*fmt))
-				precision = skip_atoi(&fmt);
-			else if (*fmt == '*') {
-				++fmt;
-				/* it's the next argument */
-				precision = va_arg(args, int);
-			}
-			if (precision < 0)
-				precision = 0;
-		}
-
-		/* get the conversion qualifier */
-		qualifier = -1;
-		if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L') {
-			qualifier = *fmt;
-			++fmt;
-		}
-
-		/* default base */
-		base = 10;
-
-		switch (*fmt) {
-		case 'c':
-			if (!(flags & LEFT))
-				while (--field_width > 0)
-					*str++ = ' ';
-			*str++ = (unsigned char)va_arg(args, int);
-			while (--field_width > 0)
-				*str++ = ' ';
-			continue;
-
-		case 's':
-			s = va_arg(args, char *);
-			len = strnlen(s, precision);
-
-			if (!(flags & LEFT))
-				while (len < field_width--)
-					*str++ = ' ';
-			for (i = 0; i < len; ++i)
-				*str++ = *s++;
-			while (len < field_width--)
-				*str++ = ' ';
-			continue;
-
-		case 'p':
-			if (field_width == -1) {
-				field_width = 2 * sizeof(void *);
-				flags |= ZEROPAD;
-			}
-			str = number(str,
-				     (unsigned long)va_arg(args, void *), 16,
-				     field_width, precision, flags);
-			continue;
-
-		case 'n':
-			if (qualifier == 'l') {
-				long *ip = va_arg(args, long *);
-				*ip = (str - buf);
-			} else {
-				int *ip = va_arg(args, int *);
-				*ip = (str - buf);
-			}
-			continue;
-
-		case '%':
-			*str++ = '%';
-			continue;
-
-			/* integer number formats - set up the flags and "break" */
-		case 'o':
-			base = 8;
-			break;
-
-		case 'x':
-			flags |= SMALL;
-		case 'X':
-			base = 16;
-			break;
-
-		case 'd':
-		case 'i':
-			flags |= SIGN;
-		case 'u':
-			break;
-
-		default:
-			*str++ = '%';
-			if (*fmt)
-				*str++ = *fmt;
-			else
-				--fmt;
-			continue;
-		}
-		if (qualifier == 'l')
-			num = va_arg(args, unsigned long);
-		else if (qualifier == 'h') {
-			num = (unsigned short)va_arg(args, int);
-			if (flags & SIGN)
-				num = (short)num;
-		} else if (flags & SIGN)
-			num = va_arg(args, int);
-		else
-			num = va_arg(args, unsigned int);
-		str = number(str, num, base, field_width, precision, flags);
-	}
-	*str = '\0';
-	return str - buf;
+int sprintf(char *out, const char *fmt, ...) {
+  // panic("Not implemented");
+  va_list ap;
+  int ret = 0;
+  va_start(ap, fmt);
+  ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -436,38 +153,5 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
-
-int sprintf(char *buf, const char *fmt, ...)
-{
-  va_list args;
-  int i;
-
-  va_start(args, fmt);
-  i = vsprintf(buf, fmt, args);
-  va_end(args);
-  return i;
-}
-
-void puts(const char *str)
-{
-	while (*str)
-		putch(*str++);
-}
-
-int printf(const char *fmt, ...)
-{
-  char printf_buf[1024];
-  va_list args;
-  int printed;
-
-  va_start(args, fmt);
-  printed = vsprintf(printf_buf, fmt, args);
-  va_end(args);
-
-  puts(printf_buf);
-
-  return printed;
-}
-
 
 #endif
