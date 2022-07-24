1eae062c1bc319b0adfcad0ed7ca383399bbbb7d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 16:48:02 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:48:02 up 33 min,  1 user,  load average: 0.72, 0.51, 0.37

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,6 +13,10 @@ Context* __am_irq_handle(Context *c) {
           ev.event = EVENT_YIELD; 
           break;
         }    
+        else if(c->GPR1 == 1){
+          ev.event = EVENT_SYSCALL; 
+          break;
+        }
       default: ev.event = EVENT_ERROR; break;
     }
     // for(int i=0;i<32;i++){
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,8 +1,11 @@
 #include <common.h>
 
+void do_syscall(Context *c);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("get yield event!!!\n"); break;
+    case EVENT_YIELD:   printf("get yield event!!!\n"); break;
+    case EVENT_SYSCALL: printf("get syscall event!!!\n"); break;//do_syscall(c);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
