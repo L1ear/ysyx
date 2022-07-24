1a045f8cafc20b8f280bbf9ec2f31050e75a6b38
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 21:18:36 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:18:36 up 18 min,  1 user,  load average: 0.39, 0.42, 0.37

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -8,7 +8,8 @@ int main() {
   while (1) {
     j ++;
     if (j == 10000) {
-      printf("Hello World from Navy-apps for the %ldth time!\n", (long)i ++);
+      printf("Hello World from Navy-apps for the %dth time!\n", i);
+      i++;
       j = 0;
     }
   }
