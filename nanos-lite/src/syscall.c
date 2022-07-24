df1bb9838bf49f86d9045ff81c8acfc987932811
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 17:56:46 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:56:46 up  1:42,  1 user,  load average: 0.50, 0.32, 0.34

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -26,10 +26,14 @@ enum {          //from lib/libos/syscall.h
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
+  a[0] = c->GPR1;   //a7
+  a[1] = c->GPR2;   //a0
+  a[2] = c->GPR3;   //a1
+  a[3] = c->GPR4;   //a2
 
   switch (a[0]) {
-    case SYS_yield: yield(); return;
+    case SYS_yield: yield(); break;
+    case SYS_exit:  halt(c->GPRx);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
