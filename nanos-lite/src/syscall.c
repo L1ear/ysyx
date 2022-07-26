a9c0fcfa0cd7b703ab997b468a6f11f53a70006b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 23:42:37 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:42:37 up  4:34,  1 user,  load average: 0.08, 0.31, 0.31

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -34,7 +34,7 @@ void do_syscall(Context *c) {
     case SYS_brk:  c->GPRx = 0; break;
     case SYS_open: c->GPRx = fs_open((void *)a[1], a[2], a[3]); break;
     case SYS_read: c->GPRx = fs_read(a[1], (void*)a[2], a[3]); break;
-    case SYS_lseek:c->GPRx = fs_lseek(a[1], (size_t)a[2], a[3]); break;
+    case SYS_lseek:printf("%llx\n",a[2]);c->GPRx = fs_lseek(a[1], (size_t)a[2], a[3]); break;
     case SYS_write:c->GPRx = fs_write(a[1], (void*)a[2], a[3]); break;
     case SYS_close:c->GPRx = fs_close(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
