dad00926843203f03a7ec8be6bcead1e2e3f9a86
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 01:03:41 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:03:41 up 10 min,  1 user,  load average: 0.40, 0.55, 0.45

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -101,7 +101,7 @@ size_t fs_read(int fd, void *buf, size_t len){
       size_t l = len <= file_table[fd].size - file_table[fd].seek_offset? len:file_table[fd].size - file_table[fd].seek_offset;
       //printf("third %d %d %d\n",file_table[fd].seek_offset,file_table[fd].size,l);
       ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].seek_offset,l);
-      printf("read %d at%d\n", buf, file_table[fd].disk_offset+file_table[fd].seek_offset);
+      printf("read %x at%d\n", buf, file_table[fd].disk_offset+file_table[fd].seek_offset);
 
       file_table[fd].seek_offset = file_table[fd].seek_offset+l;
       assert(file_table[fd].seek_offset <= file_table[fd].size);
