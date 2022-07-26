1ca62ecc97fb73ef5970e8e3d386126397792b1e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 22:12:11 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:12:11 up  3:03,  1 user,  load average: 0.41, 0.40, 0.40

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -9,14 +9,14 @@ int main() {
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-
+  printf("used fseek\n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("used fseek\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
@@ -26,7 +26,7 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("used fseek\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
