d8d9ed433cf9af84e4a775a83480a715da10f36d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 20:48:15 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:48:15 up  1:39,  1 user,  load average: 0.55, 0.41, 0.28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,12 +3,23 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+
 typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t seek_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -34,3 +45,79 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+int nr_files = sizeof(file_table)/sizeof(Finfo);
+
+int fs_open(const char *pathname, int flags, int mode){
+  int i=0;
+  for(; i<nr_files; i++){
+    if(strcmp(pathname, file_table[i].name)==0) {
+      file_table[i].seek_offset = 0;
+      break;
+    }
+  }
+  if(i == nr_files) assert(0);
+  return i;
+}
+
+size_t fs_read(int fd, void *buf, size_t len){
+//   ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].seek_offset, len);
+//   file_table[fd].seek_offset += len; 
+// }
+  if(file_table[fd].read == NULL)
+  {
+    if(file_table[fd].seek_offset >= file_table[fd].size)
+    {
+      assert(file_table[fd].seek_offset <= file_table[fd].size);
+      return 0;
+    }
+    else {
+      len = len <= file_table[fd].size - file_table[fd].seek_offset? len:file_table[fd].size - file_table[fd].seek_offset;
+      ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].seek_offset,len);
+      file_table[fd].seek_offset += len;
+      assert(file_table[fd].seek_offset <= file_table[fd].size);
+      return len;
+    }
+  }
+  else{
+    int ret = file_table[fd].read(buf,file_table[fd].seek_offset,len);
+    file_table[fd].seek_offset += len;
+    return ret;
+  }
+}
+
+int fs_close(int fd)
+{
+  return 0;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence)
+{
+  Finfo* f = &file_table[fd];
+  size_t ret=-1;
+  switch(whence){
+    case SEEK_SET:{
+      if(0<=offset && offset<=f->size){
+        f->seek_offset = offset;
+        ret=f->seek_offset;
+      }
+      break;
+    }
+    case SEEK_CUR:{
+      if((offset+f->seek_offset>=0)&&(offset+f->seek_offset<=f->size)){
+        f->seek_offset+=offset;
+        ret = f->seek_offset;
+      }
+      break;
+    }
+    case SEEK_END:{
+      if((offset+f->size>=0)&&(offset+f->size<=f->size)){
+        f->seek_offset = f->size+offset;
+        ret = f->seek_offset;
+      }
+      break;
+    }
+  }
+  return ret;
+}
+
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,34 +14,47 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
+  // Elf_Ehdr elfhdr;
+  // Elf_Phdr prohdr;
+  // ramdisk_read(&elfhdr, 0, sizeof(Elf_Ehdr));
+  // for(int i = 0;i < elfhdr.e_phnum;i++)
+  // {
+  //   ramdisk_read(&prohdr, elfhdr.e_phoff+i*sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+  //   if(prohdr.p_type == PT_LOAD){
+  //     ramdisk_read((void *)prohdr.p_vaddr, prohdr.p_offset, prohdr.p_filesz);
+  //     memset((void*)(prohdr.p_vaddr+prohdr.p_filesz),0,prohdr.p_memsz-prohdr.p_filesz);
+  //   }
+  // }
+  // return elfhdr.e_entry;
+
   Elf_Ehdr elfhdr;
   Elf_Phdr prohdr;
-  //printf("%s\n",filename);
-  // size_t fd = fs_open(filename,0,0);
-  //printf("%d\n",fd);
-  ramdisk_read(&elfhdr, 0, sizeof(Elf_Ehdr));
-  //printf("%x %x\n",elfhdr.e_phoff,elfhdr.e_phnum);
-  // assert(fd != -1);
-  //printf("%x %x %x\n",elfhdr.e_entry,elfhdr.e_phentsize,elfhdr.e_ehsize);
+  size_t fd = fs_open(filename,0,0);
+  fs_read(fd,&elfhdr,sizeof(Elf_Ehdr));
+  assert(fd != -1);
   for(int i = 0;i < elfhdr.e_phnum;i++)
   {
-    // fs_lseek(fd,elfhdr.e_phoff+i*sizeof(Elf_Phdr),SEEK_SET);
-    // fs_read(fd,&prohdr,sizeof(Elf_Phdr));
-    ramdisk_read(&prohdr, elfhdr.e_phoff+i*sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+    fs_lseek(fd,elfhdr.e_phoff+i*sizeof(Elf_Phdr),SEEK_SET);
+    fs_read(fd,&prohdr,sizeof(Elf_Phdr));
     if(prohdr.p_type == PT_LOAD){
-      // fs_lseek(fd,prohdr.p_offset,SEEK_SET);
-      // fs_read(fd,(void *)prohdr.p_vaddr,prohdr.p_filesz);
-      ramdisk_read((void *)prohdr.p_vaddr, prohdr.p_offset, prohdr.p_filesz);
-      //printf("from %x %x size = %x \n",prohdr.p_vaddr,prohdr.p_offset,prohdr.p_filesz);
+      fs_lseek(fd,prohdr.p_offset,SEEK_SET);
+      fs_read(fd,(void *)prohdr.p_vaddr,prohdr.p_filesz);
       memset((void*)(prohdr.p_vaddr+prohdr.p_filesz),0,prohdr.p_memsz-prohdr.p_filesz);
     }
     
   }
-  // fd = fs_close(fd);
-  // assert(fd == 0);
-  // printf("%s File Loaded\n",filename);
+  fd = fs_close(fd);
+  assert(fd == 0);
+  printf("%s File Loaded\n",filename);
   return elfhdr.e_entry;
 }
 
