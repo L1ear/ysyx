90ddc6dfeb9b8c5d7898691798aaa51e3051ff53
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 22:13:19 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:13:19 up  3:04,  1 user,  load average: 0.26, 0.35, 0.38

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -5,18 +5,18 @@ int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
-  printf("used fseek\n");
+  printf("used fseek to end\n");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-  printf("used fseek\n");
+  printf("used fseek to set\n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("used fseek\n");
+  printf("used fseek to set1\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
@@ -26,7 +26,7 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("used fseek\n");
+  printf("used fseek to set2\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
