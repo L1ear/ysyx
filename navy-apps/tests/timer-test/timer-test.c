3f02273d303c8c7511024db60ccc0f83e3c4af87
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 00:38:02 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:38:02 up 56 min,  1 user,  load average: 0.26, 0.49, 0.50

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -9,12 +9,13 @@ int main(){
     int hsec=5;
     while(1)
     {
-        while(tv.tv_usec/100000 <= hsec)
+        while(tv.tv_usec/100000 < hsec)
         {
             int t = gettimeofday(&tv,NULL);
             assert(t==0);
             printf("%lx\n",tv.tv_usec/100000);
         }
         printf("half sec passed\n");
+        hsec = (hsec==5)?9:5;
     } 
 }
