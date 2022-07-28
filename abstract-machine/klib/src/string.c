3d7706fa7e09363fccd449e6e8aa6976efcc8143
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 19:24:50 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:24:50 up 7 min,  1 user,  load average: 0.47, 0.65, 0.42

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,6 +4,15 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+
+size_t strnlen(const char *s, size_t count)
+{
+	const char *sc;
+	for (sc = s; count-- && *sc != '\0'; ++sc)
+		/* nothing */;
+	return sc - s;
+}
+
 size_t strlen(const char *s) {
   // panic("Not implemented");
   size_t i = 0;
