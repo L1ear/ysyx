5d1b91e235be9f2e8436708c66848477496cb5f4
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 00:22:59 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:22:59 up 41 min,  1 user,  load average: 0.61, 0.48, 0.45

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <assert.h>
-#include <time.h>
+#include <sys/time.h>
 
 int main(){
     struct timeval tv; 
     tv.tv_sec = 0;
-    printf("time-test starts begin = %d\n",tv.tv_sec);
-    int sec = 1;
+    printf("0 time");
+    int hsec=5;
     while(1)
     {
-        while(tv.tv_sec <= sec)
+        while(tv.tv_usec/100000 <= hsec)
         {
             int t = gettimeofday(&tv,NULL);
             assert(t==0);
         }
-        printf("time: seconds = %d  usec=%d usec%d\n",sec,tv.tv_sec,tv.tv_usec);
-        sec++;
+        printf("half sec passed\n");
+        hsec += 5;
     } 
 }
