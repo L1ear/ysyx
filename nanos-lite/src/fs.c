7c03eea1b66bb494832f42777270746e0642e5ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 13:14:30 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:14:30 up 59 min,  1 user,  load average: 0.26, 0.26, 0.27

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -103,7 +103,7 @@ size_t fs_read(int fd, void *buf, size_t len){
     if(f->seek_offset >= f->size)
     {
       assert(f->seek_offset <= f->size);
-      printf("%s seek_offset = %d size = %d FULL\n",f->name,f->seek_offset,f->size);
+      Log("%s seek_offset = %d size = %d FULL\n",f->name,f->seek_offset,f->size);
       return 0;
     }
     else {
@@ -111,7 +111,7 @@ size_t fs_read(int fd, void *buf, size_t len){
       size_t l = len <= file_table[fd].size - file_table[fd].seek_offset? len:file_table[fd].size - file_table[fd].seek_offset;
       //printf("third %d %d %d\n",file_table[fd].seek_offset,file_table[fd].size,l);
       ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].seek_offset,l);
-      printf("read %x at%d\n", buf, file_table[fd].disk_offset+file_table[fd].seek_offset);
+      Log("read %x at%d\n", buf, file_table[fd].disk_offset+file_table[fd].seek_offset);
 
       file_table[fd].seek_offset = file_table[fd].seek_offset+l;
       assert(file_table[fd].seek_offset <= file_table[fd].size);
