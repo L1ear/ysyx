6b4874bebad784487673574c0c5bfc985550f39e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 16:16:21 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:16:21 up  4:01,  1 user,  load average: 0.23, 0.33, 0.35

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,65 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <sys/time.h>
+#include <fcntl.h>
 #include <assert.h>
-int FB_W = -1;
-int FB_H = -1;
-void NDL_updatefb();
-int cen_off_w = 0;
-int cen_off_h = 0;
-size_t events_read(void *buf, size_t offset, size_t len);
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
-
-static struct timeval boottime;
+static int canvas_w = 0, canvas_h = 0;
+static int canvas_x = 0, canvas_y = 0;
+static int fd_disp = 0;
+static int fd_fb = 0;
+static int fd_event = 0;
 
 uint32_t NDL_GetTicks() {
-  
-  struct timeval now ;
-  gettimeofday(&now,NULL);
-  uint32_t ticks= (now.tv_sec - boottime.tv_sec)*1000 + (now.tv_usec - boottime.tv_usec)/1000 ;
-  //printf("ticks is %d\n",ticks);
-  return ticks;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
-int open(const char *path, int flags, ...);
 
 int NDL_PollEvent(char *buf, int len) {
-  //printf("NDL_Pollevent\n");
-  int fd = open("/dev/events",0,0);
-  int res = read(fd,buf,len);
-  //printf("res is %d\n",res);
-  close(fd);  
-  
-  return res;
+  assert(fd_event != 0);
+  int ret = read(fd_event, buf, len);
+  if (ret == 0)
+    return 0;
+  int i;
+  for (i = 0; i < len; i++) {
+    if (buf[i] == '\n') {
+      buf[i] = '\0';
+      return 1;
+    }
+  }
+  return 0;
 }
 
-
-
 void NDL_OpenCanvas(int *w, int *h) {
-  
-  //printf("opencanvas\n");
-    int fd = open("/proc/dispinfo",0,0);
-    char buf[128]={0};
-    read(fd,&buf,sizeof(buf));
-    //printf("buf: %s\n",buf);
-    sscanf(buf,"%*[^:]:%*[ ]%d\n%*[^:]:%*[ ]%d\n",&FB_W,&FB_H);
-    //printf("w is %d h is %d\n",FB_W,FB_H);
-    assert(FB_W >=0);
-
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
-    if(*w == 0 || *h ==0){
-      *w = FB_W;
-      *h = FB_H;
-    }
     screen_w = *w; screen_h = *h;
-    
-    
     char buf[64];
     int len = sprintf(buf, "%d %d", screen_w, screen_h);
     // let NWM resize the window and create the frame buffer
@@ -75,54 +55,36 @@ void NDL_OpenCanvas(int *w, int *h) {
     }
     close(fbctl);
   }
-  else{
-    if(*w ==0 || *h ==0){
-    *w = FB_W;
-    *h = FB_H;
-   // printf("hw == 0\n");
-    }
+  char buf[64];
+  read(fd_disp, buf, sizeof(buf));
+  sscanf(buf, "WIDTH:%d\nHEIGHT:%d", &screen_w, &screen_h);
+  if (*w > screen_w || *h > screen_h) {
+    printf("Canvas size out of screen.\n");
+    assert(0);
   }
-  close(fd);
- // printf("opencarnan W is %d H is %d\n",*w,*h);
+  if (*w == 0 && *h == 0) {
+    *w = screen_w;
+    *h = screen_h;
+  }
+  canvas_w = *w;
+  canvas_h = *h;
+  canvas_x = (screen_w - canvas_w) / 2;
+  canvas_y = (screen_h - canvas_h) / 2; // 居中画布
 }
 
-void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) { 
-
- int canvas_w = FB_W;
- int canvas_h= FB_H;
-
- // centralize
-
- 
- x += cen_off_w;
- y += cen_off_h;
- 
- 
-
- //printf("x[%d]  W[%d]  w[%d]  y[%d]  H[%d]  h[%d]\n'",x,FB_W,w,y,FB_H,h);
- int fd = open("/dev/fb",0,0);
- 
- //printf("fd is %d\n",fd);
- int offset = ((y>=0?y:0)*canvas_w + (x>=0?x:0));
- int len;
- if(x>=0){
- len = (x+w<canvas_w)?w:canvas_w-x;
- }
- else{
-   len = (x+w<canvas_w)?x+w:canvas_w;
- }
-
-
- 
- int cnt_r = 0;
-  for (int i = 0;i<h;i++){
-    if(  y+i >=0 && y+i<canvas_h){
-    lseek(fd,offset+cnt_r*(canvas_w),SEEK_SET);
-    write(fd,pixels+i*w,len);
-    cnt_r++;
-    }
+void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  int sx = x + canvas_x;
+  int sy = y + canvas_y;
+  if (sx + w > canvas_x + canvas_w)
+    w -= sx + w - (canvas_x + canvas_w);
+  if (sy + h > canvas_y + canvas_h)
+    h -= sy + h - (canvas_y + canvas_h);
+  int start = 0;
+  for (int i = 0; i < h; i++) {
+    start = ((sy + i) * screen_w + sx) * 4;
+    lseek(fd_fb, start, SEEK_SET);
+    write(fd_fb, pixels + w * i, w * 4);
   }
- close(fd);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -139,28 +101,18 @@ int NDL_QueryAudio() {
   return 0;
 }
 
-void NDL_updatefb(){
-  int fd = open("/proc/dispinfo",0,0);
-  char buf[128]={0};
- // printf("read dispinfo \n");
-  read(fd,buf,sizeof(buf));
- // printf("buf: %s\n",buf);
-  sscanf(buf,"%*[^:]:%*[ ]%d\n%*[^:]:%*[ ]%d\n",&FB_W,&FB_H);
-  //printf("w is %d h is %d\n",FB_W,FB_H);
-  assert(FB_W >=0);
-  close(fd);
-}
-
 int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
-  //printf("NDL_init\n");
-  gettimeofday(& boottime,NULL);
-  
-  NDL_updatefb();
+  char buf[64];
+  fd_disp = open("/proc/dispinfo", 0, 0);
+  fd_fb = open("/dev/fb", 0, 0);
+  fd_event = open("/dev/events", 0, 0);
   return 0;
 }
 
 void NDL_Quit() {
+  close(fd_disp);
+  close(fd_fb);
 }
