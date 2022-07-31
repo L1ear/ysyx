a5e0261c730b8be833b12ca64a9889c3b899c888
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 23:44:19 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:44:19 up  3:39,  1 user,  load average: 0.60, 0.43, 0.37

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -55,8 +55,6 @@ int NDL_PollEvent(char *buf, int len) {
   assert(0); */
 }
 
-// 打开一张(*w) X (*h)的画布
-// 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
 void NDL_OpenCanvas(int *w, int *h) {
   //printf("h = %d  w = %d\n",*h,*w);
   get_screen();
@@ -105,7 +103,6 @@ void NDL_OpenCanvas(int *w, int *h) {
   }//pa3 ignore
 }
 
-//处理dispinfo文件
 void get_screen() {
   int w = 0,h = 0;
   assert(fb_dispinfo != NULL);
@@ -156,8 +153,8 @@ void get_screen() {
   screen_w = w;
   return;
 }
-// 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
-// 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
+
+//static int k = 0;
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   assert(fb != NULL);
   if(h == 0||h > canvas_h)
