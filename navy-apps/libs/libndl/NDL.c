6df4a35ab0979295220c0a9c5a3fa5bf5fd20ee0
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 02:39:06 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:39:06 up  2:52,  1 user,  load average: 0.32, 0.38, 0.42

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -107,7 +107,7 @@ void get_screen() {
   char buf[128]={0};
   read(fb_dispinfo,buf,sizeof(buf));
   printf("%s",buf);
-  sscanf(buf,"%*[^:]:%*[ ]%d\n%*:%*[ ]%d\n",&screen_w,&screen_h);
+  sscanf(buf,"%*[^:]:%*%d\n%*:%*%d\n",&screen_w,&screen_h);
   printf("w: %d   h: %d\n", screen_w, screen_h);
   // screen_w = 400;
   // screen_h = 300;
