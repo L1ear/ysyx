a1f848da4a385d9646a431e27ee255680b27ccea
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 17:42:03 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:42:03 up  4:01,  1 user,  load average: 0.41, 0.39, 0.50

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,15 @@
 #include <common.h>
 #include "syscall.h"
 #include <sys/time.h>
+#include <proc.h>
 
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
+int sys_execve(char* filename,char * const argc[],char* const envp[]);
+void naive_uload(PCB *pcb, const char *filename);
 
 int sys_gettimeofday(struct timeval *tz,struct timezone *tv);
 
@@ -30,6 +33,31 @@ void do_syscall(Context *c) {
     case SYS_write:c->GPRx = fs_write(a[1], (const void *)a[2], a[3]); break;
     case SYS_close:c->GPRx = fs_close(a[1]); break;
     case SYS_gettimeofday: c->GPRx = sys_gettimeofday((struct timeval *)a[1],(struct timezone *)a[2]); break;
+    case SYS_execve: c->GPRx = sys_execve((char*)a[1],(char**)a[2],(char**)a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
+
+int sys_execve(char* filename,char * const argc[],char* const envp[])
+{
+  int ret = 0;
+  for(int cnt = 0;envp[cnt] != NULL;cnt+=1)
+  {
+    char* path = (char*)malloc(strlen(filename)+strlen(envp[cnt])+5);
+    strcpy(path,envp[cnt]);
+    memset(path+strlen(envp[cnt])/2+1,0,strlen(envp[cnt]));
+    path = strcat(path,"/");
+    path = strcat(path,filename);
+    //printf("%s\n",path);
+    if(fs_open(path,0,0)!= -1)
+    {
+      //printf("%s\n",path);
+      naive_uload(NULL,path);
+      break;
+    }
+  }
+  ret = fs_open(filename,0,0);
+  return ret;
+}
+
