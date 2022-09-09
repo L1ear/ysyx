8e414eabc7dbf1c18b34435c0ede9d96c8fcbf4e
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-09 16:48:40 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:48:40 up  3:54,  4 users,  load average: 0.88, 0.39, 0.30

--- npc/vsrc_pip/defines.v
+++ npc/vsrc_pip/defines.v
@@ -1,8 +1,8 @@
-import "DPI-C" function void vmemread(input longint raddr, input int len, output longint rdata, input longint pc);
-import "DPI-C" function void vmemwrite(input longint raddr, input longint wdata, input longint pc);
-import "DPI-C" function void ebreak();
-import "DPI-C" function void difftest_step(input longint pc);
-import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
+// import "DPI-C" function void vmemread(input longint raddr, input int len, output longint rdata, input longint pc);
+// import "DPI-C" function void vmemwrite(input longint raddr, input longint wdata, input longint pc);
+// import "DPI-C" function void ebreak();
+// import "DPI-C" function void difftest_step(input longint pc);
+// import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
 
 
 `define		XLEN	            64
