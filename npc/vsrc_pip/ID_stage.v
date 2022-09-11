8501bf9fc4277ddcd38fe2a1342d3a245423daff
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 02:42:58 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:42:58 up 11:30,  3 users,  load average: 0.26, 0.23, 0.21

--- npc/vsrc_pip/ID_stage.v
+++ npc/vsrc_pip/ID_stage.v
@@ -375,7 +375,7 @@ module hazard_detect (
 
 wire    hazard;
 assign  hazard = (instr_ex_i[6:2] == `load) & 
-                 (instr_id_i[6:2] == (`jalr || `branch || OP_IMM || OP_IMM_32 || OP_REG || OP_REG_32)) &
+                 (instr_id_i[6:2] == (`jalr || `branch || `OP_IMM || `OP_IMM_32 || `OP_REG || `OP_REG_32)) &
                  (instr_ex_i[11:7] == (instr_id_i[19:15] || instr_id_i[24:20]));
 assign stalln_pc = ~hazard;
 assign stalln_id = ~hazard;
