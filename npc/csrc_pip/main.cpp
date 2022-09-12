c86c074ad0321ba12b18895e826d30c4f25f5bed
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 21:43:45 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:45 up 15:49,  3 users,  load average: 0.82, 1.82, 1.21

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -110,9 +110,9 @@ void single_cycle(int i) {
     for (r = 0; r < 32; r++) {
       cpu.gpr[r] = cpu_gpr[r];
     }
-    if((cpu.pc != 0) ){ 
+    if((cpu.pc != 0) && (instr_last != 0)){ 
       // assert(0); 
-      if(en == 1 && instr_last != 115)
+      if(en == 1)
         difftest_step(cpu.pc);
       start = 0;
     }
