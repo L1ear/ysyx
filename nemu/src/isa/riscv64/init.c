136e21f09515274c4b88f4fc0f3cab717ec53095
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-17 23:32:42 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:32:42 up 15:51,  1 user,  load average: 0.33, 0.42, 0.42

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -4,11 +4,18 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-  0x00000297,  // auipc t0,0
-  0x0002b823,  // sd  zero,16(t0)
-  0x0102b503,  // ld  a0,16(t0)
-  0x00100073,  // ebreak (used as nemu_trap)
-  0xdeadbeef,  // some data
+0x30571073,//csrw	mtvec,a4
+0x342022f3,//csrr	t0,mcause
+0x30002373,//csrr	t1,mstatus
+0x341023f3,//csrr	t2,mepc
+0x30031073,//csrw	mstatus,t1
+0x34139073,//csrw	mepc,t2
+0x00000013,//addi x0,x0,0                 32   nop
+0x00000013,//addi x0,x0,0                 32   nop
+0x00000013,//addi x0,x0,0                 32   nop
+0x00000013,//addi x0,x0,0                 32   nop
+0x00000013,//addi x0,x0,0                 32   nop
+0x00100073,// ebreak (used as npctrap)
 };
 
 static void restart() {
