05a82dbb79e3b2f82eb6ec9a7d2ecf0ce0f860d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-27 14:26:11 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:26:11 up 44 min,  1 user,  load average: 0.54, 0.27, 0.20

--- npc/vsrc_pip/ID_stage.v
+++ npc/vsrc_pip/ID_stage.v
@@ -412,10 +412,10 @@ assign  hazard = ((instr_ex_i[6:0] == {`load,2'b11}) &   //此处不加两比特
                  instr_id_i[6:2] == `OP_REG || 
                  instr_id_i[6:2] == `OP_REG_32) &
                  (instr_ex_i[11:7] == instr_id_i[19:15] || 
-                 instr_ex_i[11:7] == instr_id_i[24:20]))||
-                 ((instr_ex_i[6:0] == {`load,2'b11}) & (instr_id_i[6:0] == {`load,2'b11} ||
-                  instr_id_i[6:2] == `store) &
-                  (instr_ex_i[11:7] == instr_id_i[19:15]));
+                 instr_ex_i[11:7] == instr_id_i[24:20]));//||
+                //  ((instr_ex_i[6:0] == {`load,2'b11}) & (instr_id_i[6:0] == {`load,2'b11} ||
+                //   instr_id_i[6:2] == `store) &
+                //   (instr_ex_i[11:7] == instr_id_i[19:15]));
 assign stalln_pc = ~hazard;
 assign stalln_id = ~hazard;
 assign stalln_ex = ~hazard;
