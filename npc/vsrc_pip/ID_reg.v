c7964f1302676e7e29774c2dc7c93c273d6b6dd9
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 14:24:20 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:24:20 up 14:58,  1 user,  load average: 0.23, 0.21, 0.13

--- npc/vsrc_pip/ID_reg.v
+++ npc/vsrc_pip/ID_reg.v
@@ -10,27 +10,21 @@ module ID_reg (
 );
 
 wire  [`XLEN-1:0]       pc_id_reg;
-assign pc_id_reg = (flush || in_trap_id || out_trap_id) ? `XLEN'b0 : pc_id_reg_i;
-
-always @(posedge clk or negedge rst_n) begin
-    if(~rst_n) begin
-        pc_id_reg_o <= `XLEN'b0;
-    end
-    else if(stall_n) begin
-        pc_id_reg_o <= pc_id_reg;
-    end
-end
-
 wire  [`inst_len-1:0]       instr_id_reg;
+
+assign pc_id_reg = (flush || in_trap_id || out_trap_id) ? `XLEN'b0 : pc_id_reg_i;
 assign instr_id_reg = (flush || in_trap_id || out_trap_id) ? `inst_len'b0 : instr_id_reg_i;        //为了仿真方便，就替换成0,而不是nop
 
-always @(posedge clk or negedge rst_n) begin
-    if(~rst_n) begin
-        instr_id_reg_o <= `inst_len'b0;
-    end
-    else if(stall_n) begin
-        instr_id_reg_o <= instr_id_reg;
-    end
-end
+
+stl_reg #(
+  WIDTH = 1,
+  RESET_VAL = 0
+)id_reg(
+  .i_clk   (clk),
+  .i_rst_n (rst_n),
+  .i_wen   (stall_n),
+  .i_din   ({pc_id_reg, instr_id_reg}),
+  .o_dout  ({pc_id_reg_o, instr_id_reg_o})
+);
 
 endmodule
