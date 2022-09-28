982ff538d4b4ad0a428294e10152accde0602ad9
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 14:25:22 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:25:22 up 14:59,  1 user,  load average: 0.33, 0.25, 0.15

--- npc/vsrc_pip/ID_reg.v
+++ npc/vsrc_pip/ID_reg.v
@@ -17,8 +17,8 @@ assign instr_id_reg = (flush || in_trap_id || out_trap_id) ? `inst_len'b0 : inst
 
 
 stl_reg #(
-  WIDTH = 1,
-  RESET_VAL = 0
+  WIDTH     (XLEN + inst_len),
+  RESET_VAL (0)
 )id_reg(
   .i_clk   (clk),
   .i_rst_n (rst_n),
