e9991fe39573019ad2e620b0d1ef32816dd1e80c
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-02 22:02:17 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:02:17 up  4:52,  1 user,  load average: 0.43, 0.34, 0.19

--- npc/vsrc_pip/pipline_ctrl.v
+++ npc/vsrc_pip/pipline_ctrl.v
@@ -22,7 +22,8 @@ always @(posedge clk or negedge rst_n) begin
     else
         if_valid <= if_instr_valid;
 end    
-assign  pc_stall_n = (ld_use_hazard || ~if_instr_valid) ? 1'b0 : 1'b1;
+wire    test = ld_use_hazard || ~if_instr_valid;
+assign  pc_stall_n = test ? 1'b0 : 1'b1;
 assign  id_stall_n = (ld_use_hazard || ~if_instr_valid) ? 1'b0 : 1'b1;
 assign  ex_stall_n = (!if_instr_valid) ? 1'b0 : 1'b1;
 assign  ls_stall_n = (!if_instr_valid) ? 1'b0 : 1'b1;
