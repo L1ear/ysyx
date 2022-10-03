c5b04b1bf051eee26785ffbb5875e19375a5e7ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-03 12:03:25 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:03:25 up 38 min,  1 user,  load average: 0.52, 0.37, 0.38

--- npc/vsrc_pip/IF_stage.v
+++ npc/vsrc_pip/IF_stage.v
@@ -40,7 +40,7 @@ always @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
         instr_o <= `inst_len'b0;
     end
-    else if(~stall) begin
+    else if(stall_n) begin
 assign instr_o = sram_addr[2] ? sram_rdata[63:32] : sram_rdata[31:0];
     end
 end 
