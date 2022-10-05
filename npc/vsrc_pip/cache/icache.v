55ef95ecb8adaaef7c5ddc9e0bf65c62fc70f3d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-05 11:13:31 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:13:31 up  4:12,  1 user,  load average: 0.08, 0.18, 0.27

--- npc/vsrc_pip/cache/icache.v
+++ npc/vsrc_pip/cache/icache.v
@@ -1,16 +1,32 @@
 module icache(
-    input           [`XLEN-1:0]     addr,
+    input           [`XLEN-1:0]         addr,
 
-    output                          valid
+    output                              valid,
+    output          [`inst_len-1:0]     data_o
 
 );
 
-reg     [`XLEN-1:0]     way_1[0:255];
-reg     [`XLEN-1:0]     way_2[0:255];
+reg     [`XLEN-1:0]     data_1[0:255];
+reg     [`XLEN-1:0]     data_2[0:255];
 reg     [54     :0]     tagvd_1[0:255];
 reg     [54     :0]     tagvd_2[0:255];
 
-wire    tag_cmp_1 = addr[63:11] == tagvd_1[addr[10:3]][52:0];
+wire    index = addr[10:3];
+wire    tag = addr[63:11];
+wire    tag_1 = tagvd_1[index][52:0];
+wire    tag_2 = tagvd_2[index][52:0];
+
+wire    hit_1 = (tag == tag_1) && tagvd_1[addr[10:3]][53];
+wire    hit_2 = (tag == tag_2) && tagvd_2[addr[10:3]][53];
+assign  valid = hit_1 || hit_2;
+
+assign  data_o = ({64{hit_1}} && data_1[index]) ||
+                 ({64{hit_2}} && data_2[index]);
+// always@()begin
+
+// end
+
+
 
 
 
