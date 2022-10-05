e2bc7549e3357f853bc0d128dda143222b45d482
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-05 11:00:59 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:00:59 up  3:59,  1 user,  load average: 0.33, 0.35, 0.36

--- npc/vsrc_pip/cache/icache.v
+++ npc/vsrc_pip/cache/icache.v
@@ -1,12 +1,20 @@
-// module icache(
-//     input       
-// );
+module icache(
+    input           [`XLEN-1:0]     addr,
+
+    output                          valid
+
+);
+
+reg     [`XLEN-1:0]     way_1[0:255];
+reg     [`XLEN-1:0]     way_2[0:255];
+reg     [54     :0]     tagvd_1[0:255];
+reg     [54     :0]     tagvd_2[0:255];
+
+wire    tag_cmp_1[63:11] = addr == tagvd_1[addr[10:3]][52:0];
 
 
 
 
 
 
-
-
-// endmodule
+endmodule
