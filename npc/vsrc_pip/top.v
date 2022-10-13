7b30f38efa9032187f51bbf3a4170c9e79b5e681
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 18:28:36 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:28:36 up  2:02,  1 user,  load average: 0.64, 0.55, 0.42

--- npc/vsrc_pip/top.v
+++ npc/vsrc_pip/top.v
@@ -42,7 +42,7 @@ module top # (
     input  [AXI_DATA_WIDTH-1:0]         axi_r_data_i,       //lite
     input                               axi_r_last_i,
     input  [AXI_ID_WIDTH-1:0]           axi_r_id_i,
-    input  [AXI_USER_WIDTH-1:0]         axi_r_user_i
+    input  [AXI_USER_WIDTH-1:0]         axi_r_user_i,
 
 //ls sram interface
     output          [`XLEN-1:0]     ls_sram_addr,
