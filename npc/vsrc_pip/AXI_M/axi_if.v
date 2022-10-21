69c21d303ced3f74d22042f5bfc20ee2b0f07683
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-21 09:12:09 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:12:09 up 16:28,  1 user,  load average: 0.37, 0.35, 0.33

--- npc/vsrc_pip/AXI_M/axi_if.v
+++ npc/vsrc_pip/AXI_M/axi_if.v
@@ -169,10 +169,9 @@ always@(posedge clock) begin
         instr_valid_reg <= 1'b1;
         rd_data_reg <= axi_r_data_i;
     end
-    // else if(r_state != r_state_r_wait) begin
-    //     instr_valid <= 1'b0;
-    //     rd_data_reg <= `XLEN'b0;;
-    // end
+    else if(r_state != r_state_trans_ok) begin
+        instr_valid_reg <= 1'b0;
+    end
 end
 always@(*) begin
     case (r_state)
