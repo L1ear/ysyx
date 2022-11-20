9dce5ed36816ad125a2b0bc532a72aa14f9e96bb
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-20 20:24:43 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:24:43 up  3:30,  1 user,  load average: 1.01, 0.66, 0.49

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -119,8 +119,8 @@ int main(int argc, char *argv[])
     axi4_ref <63,64,4> mmio_sigs_ref(mmio_sigs);
     axi4_xbar<63,64,4> mmio;   
     
-    uartlite           uart;
-    std::thread        uart_input_thread(uart_input,std::ref(uart));
+    // uartlite           uart;
+    // std::thread        uart_input_thread(uart_input,std::ref(uart));
     // assert(mmio.add_dev(0x60100000,1024*1024,&uart));
 
     Log("axi check complete!");
