5dca93ec0c096cd473c97a3941bf8596a8016987
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-21 09:31:47 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:31:47 up 54 min,  1 user,  load average: 0.89, 0.85, 0.77

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -209,9 +209,7 @@ char  stall;
 void single_cycle(int i) {
 //上升沿
   top->clk = 1; 
-  printf("b\n");
   mmio_sigs.update_input(*mmioref);
-  printf("a\n");
   mem_sigs.update_input(*memref);
   top->eval();
   
