5cd6df516eb74a78aa8baebb1bc1ac01be5b8501
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-07 03:42:31 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:42:31 up  8:36,  1 user,  load average: 1.24, 0.66, 0.45

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -343,7 +343,7 @@ void ebreak(){
 extern "C"  void axiSlaveRead(long long raddr, char size, long long* rdata){
       // printf("addr:%016llx \nsize: %d\ndata: %016llx\n",raddr, 2^size, memread(raddr & ~0x7ull, 8, pc));
       // assert(0);
-      *rdata = memread(raddr & ~0x7ull, (uint8_t)8, pc);
+      *rdata = memread(raddr & ~0x7ull, pow(2,(double)size), pc);
 }
 extern "C"  void axiSlaveWrite(long long waddr, long long wdata, char wmask){
 
