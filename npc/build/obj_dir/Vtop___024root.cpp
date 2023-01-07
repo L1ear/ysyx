acacc3791810d148312631b44e3938d775b419a0
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-07 14:42:47 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:42:47 up 11:02,  1 user,  load average: 2.48, 1.24, 0.94

--- npc/build/obj_dir/Vtop___024root.cpp
+++ npc/build/obj_dir/Vtop___024root.cpp
@@ -3641,53 +3641,7 @@ void Vtop___024root___eval_debug_assertions(Vtop___024root* vlSelf) {
         Verilated::overWidthError("clk");}
     if (VL_UNLIKELY((vlSelf->rst_n & 0xfeU))) {
         Verilated::overWidthError("rst_n");}
-    if (VL_UNLIKELY((vlSelf->axi_ar_ready_i & 0xfeU))) {
-        Verilated::overWidthError("axi_ar_ready_i");}
-    if (VL_UNLIKELY((vlSelf->axi_r_valid_i & 0xfeU))) {
-        Verilated::overWidthError("axi_r_valid_i");}
-    if (VL_UNLIKELY((vlSelf->axi_r_resp_i & 0xfcU))) {
-        Verilated::overWidthError("axi_r_resp_i");}
-    if (VL_UNLIKELY((vlSelf->axi_r_last_i & 0xfeU))) {
-        Verilated::overWidthError("axi_r_last_i");}
-    if (VL_UNLIKELY((vlSelf->axi_r_id_i & 0xf0U))) {
-        Verilated::overWidthError("axi_r_id_i");}
-    if (VL_UNLIKELY((vlSelf->axi_r_user_i & 0xfeU))) {
-        Verilated::overWidthError("axi_r_user_i");}
-    if (VL_UNLIKELY((vlSelf->axi_aw_ready_i & 0xfeU))) {
-        Verilated::overWidthError("axi_aw_ready_i");}
-    if (VL_UNLIKELY((vlSelf->axi_w_ready_i & 0xfeU))) {
-        Verilated::overWidthError("axi_w_ready_i");}
-    if (VL_UNLIKELY((vlSelf->axi_b_valid_i & 0xfeU))) {
-        Verilated::overWidthError("axi_b_valid_i");}
-    if (VL_UNLIKELY((vlSelf->axi_b_resp_i & 0xfcU))) {
-        Verilated::overWidthError("axi_b_resp_i");}
-    if (VL_UNLIKELY((vlSelf->axi_b_id_i & 0xf0U))) {
-        Verilated::overWidthError("axi_b_id_i");}
-    if (VL_UNLIKELY((vlSelf->axi_b_user_i & 0xfeU))) {
-        Verilated::overWidthError("axi_b_user_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_ar_ready_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_ar_ready_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_r_valid_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_r_valid_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_r_resp_i & 0xfcU))) {
-        Verilated::overWidthError("axi_mmio_r_resp_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_r_last_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_r_last_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_r_id_i & 0xf0U))) {
-        Verilated::overWidthError("axi_mmio_r_id_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_r_user_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_r_user_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_aw_ready_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_aw_ready_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_w_ready_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_w_ready_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_b_valid_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_b_valid_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_b_resp_i & 0xfcU))) {
-        Verilated::overWidthError("axi_mmio_b_resp_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_b_id_i & 0xf0U))) {
-        Verilated::overWidthError("axi_mmio_b_id_i");}
-    if (VL_UNLIKELY((vlSelf->axi_mmio_b_user_i & 0xfeU))) {
-        Verilated::overWidthError("axi_mmio_b_user_i");}
+    if (VL_UNLIKELY((vlSelf->__pinNumber8 & 0xfeU))) {
+        Verilated::overWidthError("__pinNumber8");}
 }
 #endif  // VL_DEBUG
