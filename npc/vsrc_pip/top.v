441deb18653c2ac9695ce0c718024f8c0d6d5e2e
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-07 14:42:47 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:42:47 up 11:02,  1 user,  load average: 2.48, 1.24, 0.94

--- npc/vsrc_pip/top.v
+++ npc/vsrc_pip/top.v
@@ -22,101 +22,101 @@ module top # (
     // output                          sram_ren,
     // output                          sram_addr_valid,
 
-    input                               axi_ar_ready_i,     //lite              
-    output                              axi_ar_valid_o,     //lite
-    output [AXI_ADDR_WIDTH-1:0]         axi_ar_addr_o,      //lite
-    output [2:0]                        axi_ar_prot_o,
-    output [AXI_ID_WIDTH-1:0]           axi_ar_id_o,
-    output [AXI_USER_WIDTH-1:0]         axi_ar_user_o,
-    output [7:0]                        axi_ar_len_o,       //lite
-    output [2:0]                        axi_ar_size_o,      //lite
-    output [1:0]                        axi_ar_burst_o,
-    output                              axi_ar_lock_o,
-    output [3:0]                        axi_ar_cache_o,
-    output [3:0]                        axi_ar_qos_o,
-    output [3:0]                        axi_ar_region_o,
+    // input                               axi_ar_ready_i,     //lite              
+    // output                              axi_ar_valid_o,     //lite
+    // output [AXI_ADDR_WIDTH-1:0]         axi_ar_addr_o,      //lite
+    // output [2:0]                        axi_ar_prot_o,
+    // output [AXI_ID_WIDTH-1:0]           axi_ar_id_o,
+    // output [AXI_USER_WIDTH-1:0]         axi_ar_user_o,
+    // output [7:0]                        axi_ar_len_o,       //lite
+    // output [2:0]                        axi_ar_size_o,      //lite
+    // output [1:0]                        axi_ar_burst_o,
+    // output                              axi_ar_lock_o,
+    // output [3:0]                        axi_ar_cache_o,
+    // output [3:0]                        axi_ar_qos_o,
+    // output [3:0]                        axi_ar_region_o,
     
-    output                              axi_r_ready_o,      //lite            
-    input                               axi_r_valid_i,      //lite            
-    input  [1:0]                        axi_r_resp_i,
-    input  [AXI_DATA_WIDTH-1:0]         axi_r_data_i,       //lite
-    input                               axi_r_last_i,
-    input  [AXI_ID_WIDTH-1:0]           axi_r_id_i,
-    input  [AXI_USER_WIDTH-1:0]         axi_r_user_i,
+    // output                              axi_r_ready_o,      //lite            
+    // input                               axi_r_valid_i,      //lite            
+    // input  [1:0]                        axi_r_resp_i,
+    // input  [AXI_DATA_WIDTH-1:0]         axi_r_data_i,       //lite
+    // input                               axi_r_last_i,
+    // input  [AXI_ID_WIDTH-1:0]           axi_r_id_i,
+    // input  [AXI_USER_WIDTH-1:0]         axi_r_user_i,
 
-    input                               axi_aw_ready_i,     //lite        
-    output                              axi_aw_valid_o,     //lite
-    output [AXI_ADDR_WIDTH-1:0]         axi_aw_addr_o,      //lite
-    output [2:0]                        axi_aw_prot_o,
-    output [AXI_ID_WIDTH-1:0]           axi_aw_id_o,
-    output [AXI_USER_WIDTH-1:0]         axi_aw_user_o,
-    output [7:0]                        axi_aw_len_o,       
-    output [2:0]                        axi_aw_size_o,
-    output [1:0]                        axi_aw_burst_o,
-    output                              axi_aw_lock_o,
-    output [3:0]                        axi_aw_cache_o,
-    output [3:0]                        axi_aw_qos_o,
-    output [3:0]                        axi_aw_region_o,
-    input                               axi_w_ready_i,      //lite        
-    output                              axi_w_valid_o,      //lite
-    output [AXI_DATA_WIDTH-1:0]         axi_w_data_o,       //lite
-    output [AXI_DATA_WIDTH/8-1:0]       axi_w_strb_o,       //lite
-    output                              axi_w_last_o,
-    output [AXI_USER_WIDTH-1:0]         axi_w_user_o,
+    // input                               axi_aw_ready_i,     //lite        
+    // output                              axi_aw_valid_o,     //lite
+    // output [AXI_ADDR_WIDTH-1:0]         axi_aw_addr_o,      //lite
+    // output [2:0]                        axi_aw_prot_o,
+    // output [AXI_ID_WIDTH-1:0]           axi_aw_id_o,
+    // output [AXI_USER_WIDTH-1:0]         axi_aw_user_o,
+    // output [7:0]                        axi_aw_len_o,       
+    // output [2:0]                        axi_aw_size_o,
+    // output [1:0]                        axi_aw_burst_o,
+    // output                              axi_aw_lock_o,
+    // output [3:0]                        axi_aw_cache_o,
+    // output [3:0]                        axi_aw_qos_o,
+    // output [3:0]                        axi_aw_region_o,
+    // input                               axi_w_ready_i,      //lite        
+    // output                              axi_w_valid_o,      //lite
+    // output [AXI_DATA_WIDTH-1:0]         axi_w_data_o,       //lite
+    // output [AXI_DATA_WIDTH/8-1:0]       axi_w_strb_o,       //lite
+    // output                              axi_w_last_o,
+    // output [AXI_USER_WIDTH-1:0]         axi_w_user_o,
         
-    output                              axi_b_ready_o,      //lite        
-    input                               axi_b_valid_i,      //lite
-    input  [1:0]                        axi_b_resp_i,       //lite        
-    input  [AXI_ID_WIDTH-1:0]           axi_b_id_i,
-    input  [AXI_USER_WIDTH-1:0]         axi_b_user_i,
+    // output                              axi_b_ready_o,      //lite        
+    // input                               axi_b_valid_i,      //lite
+    // input  [1:0]                        axi_b_resp_i,       //lite        
+    // input  [AXI_ID_WIDTH-1:0]           axi_b_id_i,
+    // input  [AXI_USER_WIDTH-1:0]         axi_b_user_i,
 
-    input                               axi_mmio_ar_ready_i,     //lite              
-    output                              axi_mmio_ar_valid_o,     //lite
-    output [AXI_ADDR_WIDTH-1:0]         axi_mmio_ar_addr_o,      //lite
-    output [2:0]                        axi_mmio_ar_prot_o,
-    output [AXI_ID_WIDTH-1:0]           axi_mmio_ar_id_o,
-    output [AXI_USER_WIDTH-1:0]         axi_mmio_ar_user_o,
-    output [7:0]                        axi_mmio_ar_len_o,       //lite
-    output [2:0]                        axi_mmio_ar_size_o,      //lite
-    output [1:0]                        axi_mmio_ar_burst_o,
-    output                              axi_mmio_ar_lock_o,
-    output [3:0]                        axi_mmio_ar_cache_o,
-    output [3:0]                        axi_mmio_ar_qos_o,
-    output [3:0]                        axi_mmio_ar_region_o,
+    // input                               axi_mmio_ar_ready_i,     //lite              
+    // output                              axi_mmio_ar_valid_o,     //lite
+    // output [AXI_ADDR_WIDTH-1:0]         axi_mmio_ar_addr_o,      //lite
+    // output [2:0]                        axi_mmio_ar_prot_o,
+    // output [AXI_ID_WIDTH-1:0]           axi_mmio_ar_id_o,
+    // output [AXI_USER_WIDTH-1:0]         axi_mmio_ar_user_o,
+    // output [7:0]                        axi_mmio_ar_len_o,       //lite
+    // output [2:0]                        axi_mmio_ar_size_o,      //lite
+    // output [1:0]                        axi_mmio_ar_burst_o,
+    // output                              axi_mmio_ar_lock_o,
+    // output [3:0]                        axi_mmio_ar_cache_o,
+    // output [3:0]                        axi_mmio_ar_qos_o,
+    // output [3:0]                        axi_mmio_ar_region_o,
   
-    output                              axi_mmio_r_ready_o,      //lite            
-    input                               axi_mmio_r_valid_i,      //lite            
-    input  [1:0]                        axi_mmio_r_resp_i,
-    input  [AXI_DATA_WIDTH-1:0]         axi_mmio_r_data_i,       //lite
-    input                               axi_mmio_r_last_i,
-    input  [AXI_ID_WIDTH-1:0]           axi_mmio_r_id_i,
-    input  [AXI_USER_WIDTH-1:0]         axi_mmio_r_user_i,
+    // output                              axi_mmio_r_ready_o,      //lite            
+    // input                               axi_mmio_r_valid_i,      //lite            
+    // input  [1:0]                        axi_mmio_r_resp_i,
+    // input  [AXI_DATA_WIDTH-1:0]         axi_mmio_r_data_i,       //lite
+    // input                               axi_mmio_r_last_i,
+    // input  [AXI_ID_WIDTH-1:0]           axi_mmio_r_id_i,
+    // input  [AXI_USER_WIDTH-1:0]         axi_mmio_r_user_i,
 
-    input                               axi_mmio_aw_ready_i,     //lite        
-    output                              axi_mmio_aw_valid_o,     //lite
-    output [AXI_ADDR_WIDTH-1:0]         axi_mmio_aw_addr_o,      //lite
-    output [2:0]                        axi_mmio_aw_prot_o,
-    output [AXI_ID_WIDTH-1:0]           axi_mmio_aw_id_o,
-    output [AXI_USER_WIDTH-1:0]         axi_mmio_aw_user_o,
-    output [7:0]                        axi_mmio_aw_len_o,       
-    output [2:0]                        axi_mmio_aw_size_o,
-    output [1:0]                        axi_mmio_aw_burst_o,
-    output                              axi_mmio_aw_lock_o,
-    output [3:0]                        axi_mmio_aw_cache_o,
-    output [3:0]                        axi_mmio_aw_qos_o,
-    output [3:0]                        axi_mmio_aw_region_o,
-    input                               axi_mmio_w_ready_i,      //lite        
-    output                              axi_mmio_w_valid_o,      //lite
-    output [AXI_DATA_WIDTH-1:0]         axi_mmio_w_data_o,       //lite
-    output [AXI_DATA_WIDTH/8-1:0]       axi_mmio_w_strb_o,       //lite
-    output                              axi_mmio_w_last_o,
-    output [AXI_USER_WIDTH-1:0]         axi_mmio_w_user_o,
+    // input                               axi_mmio_aw_ready_i,     //lite        
+    // output                              axi_mmio_aw_valid_o,     //lite
+    // output [AXI_ADDR_WIDTH-1:0]         axi_mmio_aw_addr_o,      //lite
+    // output [2:0]                        axi_mmio_aw_prot_o,
+    // output [AXI_ID_WIDTH-1:0]           axi_mmio_aw_id_o,
+    // output [AXI_USER_WIDTH-1:0]         axi_mmio_aw_user_o,
+    // output [7:0]                        axi_mmio_aw_len_o,       
+    // output [2:0]                        axi_mmio_aw_size_o,
+    // output [1:0]                        axi_mmio_aw_burst_o,
+    // output                              axi_mmio_aw_lock_o,
+    // output [3:0]                        axi_mmio_aw_cache_o,
+    // output [3:0]                        axi_mmio_aw_qos_o,
+    // output [3:0]                        axi_mmio_aw_region_o,
+    // input                               axi_mmio_w_ready_i,      //lite        
+    // output                              axi_mmio_w_valid_o,      //lite
+    // output [AXI_DATA_WIDTH-1:0]         axi_mmio_w_data_o,       //lite
+    // output [AXI_DATA_WIDTH/8-1:0]       axi_mmio_w_strb_o,       //lite
+    // output                              axi_mmio_w_last_o,
+    // output [AXI_USER_WIDTH-1:0]         axi_mmio_w_user_o,
         
-    output                              axi_mmio_b_ready_o,      //lite        
-    input                               axi_mmio_b_valid_i,      //lite
-    input  [1:0]                        axi_mmio_b_resp_i,       //lite        
-    input  [AXI_ID_WIDTH-1:0]           axi_mmio_b_id_i,
-    input  [AXI_USER_WIDTH-1:0]         axi_mmio_b_user_i
+    // output                              axi_mmio_b_ready_o,      //lite        
+    // input                               axi_mmio_b_valid_i,      //lite
+    // input  [1:0]                        axi_mmio_b_resp_i,       //lite        
+    // input  [AXI_ID_WIDTH-1:0]           axi_mmio_b_id_i,
+    // input  [AXI_USER_WIDTH-1:0]         axi_mmio_b_user_i
 
 // //ls sram interface
 //     output          [`XLEN-1:0]     ls_sram_addr,
