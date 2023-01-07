b3848b688b00b4d8568e1c59fb1b1f333894a859
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-07 18:36:35 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:36:35 up 14:55,  1 user,  load average: 0.52, 0.60, 0.54

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -26,7 +26,7 @@ void device_update() {
   }
   last = now;
 
-  // IFDEF(CONFIG_HAS_VGA, vga_update_screen());
+  IFDEF(CONFIG_HAS_VGA, vga_update_screen());
 
 #ifndef CONFIG_TARGET_AM
   SDL_Event event;
@@ -64,7 +64,7 @@ void init_device() {
 
   IFDEF(CONFIG_HAS_SERIAL, init_serial());
   IFDEF(CONFIG_HAS_TIMER, init_timer());
-  IFDEF(CONFIG_HAS_VGA, init_vga());
+  // IFDEF(CONFIG_HAS_VGA, init_vga());
   IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
   IFDEF(CONFIG_HAS_AUDIO, init_audio());
   IFDEF(CONFIG_HAS_DISK, init_disk());
