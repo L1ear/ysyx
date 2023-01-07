4cef5b356608dbda5ac14e5a37a821c8f101b2f5
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-08 00:42:21 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:42:21 up 18:46,  1 user,  load average: 0.79, 0.66, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,18 +23,18 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t *pixels = (uint32_t*)(ctl->pixels);
-  int x = ctl->x, y = ctl->y;
-  int w = ctl->w, h = ctl->h;
-  for(int j = 0; j< h; j++){
-    for(int i = 0; i< w; i++){
-      fb[(y+j)*W+(x+i)] = *(pixels+j*w+i);
-    }
-  }
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // uint32_t *pixels = (uint32_t*)(ctl->pixels);
+  // int x = ctl->x, y = ctl->y;
+  // int w = ctl->w, h = ctl->h;
+  // for(int j = 0; j< h; j++){
+  //   for(int i = 0; i< w; i++){
+  //     fb[(y+j)*W+(x+i)] = *(pixels+j*w+i);
+  //   }
+  // }
+  // if (ctl->sync) {
+  //   outl(SYNC_ADDR, 1);
+  // }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
