f1cf92ab1efe8f7a53ed4715641471d1b553c99e
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-11 15:49:45 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:49:45 up 1 day,  2:45,  1 user,  load average: 0.80, 0.71, 0.54

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -341,7 +341,8 @@ void ebreak(){
   en = 0;
   if(top->regA0 == 0){
     Log("npc: \33[1;32mHIT GOOD TRAP\33[0m at pc = %08x\n",cpu.pc);
-    Log("after %d instructions", nr_instr);
+    Log("after %d instructions and %d clock cycle", nr_instr, nr_cycle);
+    Log("IPC: %f", nr_instr/nr_cycle);
   }
     
   else
