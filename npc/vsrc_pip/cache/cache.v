e5513bd2d7f65aeb65be30a4f09f29e9baf6826d
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-13 12:09:48 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:09:48 up 1 day, 19:05,  1 user,  load average: 0.12, 0.03, 0.08

--- npc/vsrc_pip/cache/cache.v
+++ npc/vsrc_pip/cache/cache.v
@@ -106,8 +106,8 @@ reg        bitValid1_d,bitValid2_d;
 //TODO
 always @(posedge clk or negedge rst_n) begin
     if(getdataEn) begin
-        validArray1[index] <= bitValid1_d;
-        validArray2[index] <= bitValid2_d;
+        validArray1[index] = bitValid1_d;
+        validArray2[index] = bitValid2_d;
     end
 end
 always @(posedge clk or negedge rst_n) begin
