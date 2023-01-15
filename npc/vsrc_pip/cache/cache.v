2e39e1eedff6c443d39deaa3eaae4ff666727866
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 00:47:41 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:47:41 up 2 days, 15:51,  1 user,  load average: 0.82, 0.66, 0.45

--- npc/vsrc_pip/cache/cache.v
+++ npc/vsrc_pip/cache/cache.v
@@ -259,14 +259,14 @@ always @(*) begin
         tagArray2_d = 'b0;
     end
 end
-
+wire    replaceEn = cacheCurState == replace;
 //延后一个周期写入，防止高位无法写入（即最后64位数据）
 always @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
         wenWay1 <= 1'b0;
         wenWay2 <= 1'b0;
     end
-    else if(getdataEn && rdLast_i) begin
+    else if(replaceEn) begin
         if(randomBit[0]) begin
             wenWay1 <= 1'b1;
             wenWay2 <= 1'b0;
