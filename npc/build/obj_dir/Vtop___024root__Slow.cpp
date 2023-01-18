76833b7a57b7ed27592a770184bbfcee32bc3cb4
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 09:50:26 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:50:26 up  4:31,  1 user,  load average: 0.10, 0.09, 0.16

--- npc/build/obj_dir/Vtop___024root__Slow.cpp
+++ npc/build/obj_dir/Vtop___024root__Slow.cpp
@@ -3597,13 +3597,13 @@ void Vtop___024root___settle__TOP__1(Vtop___024root* vlSelf) {
                                         << 0x20U) | (QData)((IData)(vlSelf->top__DOT__ex_stage_u__DOT__u_ALU__DOT__ALUout)))
                                     : vlSelf->top__DOT__ex_stage_u__DOT__u_ALU__DOT__ALUout);
     vlSelf->top__DOT____Vcellinp__Dcache_u__addr_i 
-        = (IData)((((3U == (0x7fU & ((vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[7U] 
-                                      << 5U) | (vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[6U] 
-                                                >> 0x1bU)))) 
-                    | (8U == (0x1fU & ((vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[7U] 
-                                        << 3U) | (vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[6U] 
-                                                  >> 0x1dU)))))
-                    ? vlSelf->top__DOT__alures_ex : 0ULL));
+        = (((3U == (0x7fU & ((vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[7U] 
+                              << 5U) | (vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[6U] 
+                                        >> 0x1bU)))) 
+            | (8U == (0x1fU & ((vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[7U] 
+                                << 3U) | (vlSelf->top__DOT__EX_reg_u__DOT____Vcellout__ex_reg__o_dout[6U] 
+                                          >> 0x1dU)))))
+            ? (IData)(vlSelf->top__DOT__alures_ex) : 0U);
     if (vlSelf->top__DOT__Dcache_u__DOT__wenWay1) {
         vlSelf->top__DOT__Dcache_u__DOT____Vcellinp__iramWay1_1__A 
             = (0x3fU & (IData)((vlSelf->top__DOT__Dcache_u__DOT__reqLatch 
