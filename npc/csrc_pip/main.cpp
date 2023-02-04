fb849443c6ef955311cbe8ce1917540267f21903
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-05 02:12:35 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:12:35 up 12:01,  1 user,  load average: 3.02, 2.77, 2.40

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -299,7 +299,7 @@ void single_cycle(int i) {
       if(en == 1 )
         {
 #ifdef  difftest
-          if(instr_last == 0x3ea78c23 ||instr_last == 0x0487b783){    //跳过printf和读取时间
+          if(instr_last == 0x3ea78e23 ||instr_last == 0x0487b783){    //跳过printf和读取时间
             difftest_skip_ref();
           }
           Log("%08lx",instr_last);
