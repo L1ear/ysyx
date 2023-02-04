8a64db43ffedb5c46f174f9f9393524159a86b74
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 15:48:15 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:48:15 up  7:14,  1 user,  load average: 0.16, 0.12, 0.17

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -172,7 +172,7 @@ int main(int argc, char *argv[])
       
     
     
-    std::thread uart_input_thread(uart_input,std::ref(uart));
+    // std::thread uart_input_thread(uart_input,std::ref(uart));
     assert(mmio.add_dev(0xa00003f8,8,&uart));
 
     Log("axi check complete!");
@@ -193,7 +193,7 @@ int main(int argc, char *argv[])
 
     en = 1;
     sdb_mainloop();
-    uart_input_thread.join();
+    // uart_input_thread.join();
     top->final();
     
     // while(en)
