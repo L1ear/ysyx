4c3bfeaf7cfc84d6346e497303073cb8cbb5d662
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 19:16:05 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:16:05 up 21:41,  1 user,  load average: 2.32, 2.32, 2.41

--- npc/csrc_pip/mem.cpp
+++ npc/csrc_pip/mem.cpp
@@ -78,6 +78,7 @@ long load_img() {
   uint64_t buf=0xdeadbeef;
   mem.write(0x8009df20+24,8,(uint8_t *)&buf);
   buf = 0;
+  printf("0x8009df20+24:%lx\n", buf);
   mem.read(0x8009df20+24,8,(uint8_t *)&buf);
   printf("0x8009df20+24:%lx\n", buf);
   // assert(0);
