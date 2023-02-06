73097e7c01f11dfd97f9d90e5ae66034a0512830
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 18:21:32 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:21:32 up 20:46,  1 user,  load average: 2.88, 2.23, 2.45

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -285,7 +285,8 @@ void single_cycle(int i) {
   // }
   top->eval();
 #ifdef vcd
-  if(i>=5400000) fp ->dump(i);
+  //if(i>=5400000) 
+    fp ->dump(i);
 #endif
 
 #ifdef  difftest    
@@ -320,7 +321,8 @@ void single_cycle(int i) {
   top->clk = 0;
   top->eval();
 #ifdef vcd
-  if(i>=5400000) fp ->dump(i+1);
+  //if(i>=5400000) 
+    fp ->dump(i+1);
 #endif
   // pc = top->pc_decoding;
 #ifdef  difftest
