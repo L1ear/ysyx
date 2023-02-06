2ec68664c9b2c848624bc72d6444f8543fb49cda
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 14:23:12 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:23:12 up 19:40,  1 user,  load average: 2.09, 2.16, 2.28

--- npc/vsrc_pip/cache/Dcache.v
+++ npc/vsrc_pip/cache/Dcache.v
@@ -155,7 +155,7 @@ always @(*) begin
         end
         uncacheOp: begin
             if(uncacheOpOk && stall_n) begin
-                cacheNexState = compare;
+                cacheNexState = idle;
             end
             else begin
                 cacheNexState = uncacheOp;
@@ -179,14 +179,14 @@ always @(posedge clk or negedge rst_n) begin
     //在compare到compare锁存地址信息时，要保证上一个请求是hit的，否则下一拍会进入miss，而保存的数据失效
     //同时要保证在stall时不锁存，因为1、stall有可能是由cache缺失或其他自身原因造成，此时不能锁存其他数据
     //2、有可能由其他阶段造成如ls部分stall等，此时也不能锁存，否则会锁存下一拍的地址，但是pc还没有变化，导致取得的指令出错
-    else if(((idleEn && exValid_i && stall_n) || ((compareEn && exValid_i && cacheHit) && stall_n) || (uncacheOpEn && exValid_i && stall_n))) begin
+    else if(((idleEn && exValid_i && stall_n) || (compareEn && exValid_i && cacheHit) && stall_n)) begin
         reqLatch <= {op_i,addr_i};
     end
 end
 
 //addrOk信号仅在idle或者compare且上一拍pc命中的情况下为高，表示新的pc可以被接收
 always @(*) begin
-    if(idleEn || (compareEn && cacheHit) || (uncacheOpEn && uncacheOpOk)) begin //可能要加上stall？
+    if(idleEn || (compareEn && cacheHit)) begin
         addr_ok_o = 1'b1;
     end
     else begin
@@ -252,7 +252,7 @@ assign data_ok_o = compareEn && cacheHit;
 **  防止读出错误数据，本质上是read after write冲突，本应该使用流水线前递解决，整理完代码再改吧
 **
 */
-assign data_notok_o = (uncacheOpEn && ~uncacheOpOk) || (compareEn && ~cacheHit) || getdataEn || missEn || replaceEn || (compareEn && ~reqLatch[32] && ~replaceEnDelay && ((way1Hit && wenDelay1) || (way2Hit && wenDelay2)));
+assign data_notok_o = (uncacheOpEn && ~uncacheOpOk) || (compareEn && ~cacheHit) || idleEn || getdataEn || missEn || replaceEn || (compareEn && ~reqLatch[32] && ~replaceEnDelay && ((way1Hit && wenDelay1) || (way2Hit && wenDelay2)));
 
 reg     replaceEnDelay;
 always @(posedge clk or negedge rst_n) begin
