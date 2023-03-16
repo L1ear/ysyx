26d087b2aa8a30face9f9393d25c940657d7382b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 23:20:05 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:20:05 up 12:53,  1 user,  load average: 1.39, 1.03, 0.62

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:   /*printf("get yield event!!!\n");*/ break;
-    case EVENT_SYSCALL: printf("get syscall event!!!, num:%d\n", c->GPR1); do_syscall(c); break;
+    case EVENT_SYSCALL: /*printf("get syscall event!!!, num:%d\n", c->GPR1);*/ do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
