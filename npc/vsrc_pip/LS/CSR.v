932ca8cab43d474b829daa15306759ffadb00c43
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 01:29:12 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:29:12 up 15:08,  1 user,  load average: 0.46, 0.40, 0.37

--- npc/vsrc_pip/LS/CSR.v
+++ npc/vsrc_pip/LS/CSR.v
@@ -59,7 +59,7 @@ always @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
         mepc <= `XLEN'b0;
     end
-    else if((sel_mepc | trap ) && stall_n) begin
+    else if((sel_mepc | trap | in_intr_ls) && stall_n) begin
         mepc[`XLEN-1:2]<= (trap || in_intr_ls) ? pc_i[`XLEN-1:2] : wr_data[`XLEN-1:2];
         //interupt时pc+4
     end
@@ -84,7 +84,7 @@ always @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
         mstatus <= `XLEN'ha00001808;
     end
-    else if((sel_mstatus | trap) && stall_n) begin
+    else if((sel_mstatus | trap | in_intr_ls) && stall_n) begin
         mstatus <= (trap || in_intr_ls) ? (system & ~instr_i[21] & (instr_i[14:12]==3'b0)) ? {mstatus[`XLEN-1:13],2'b11,mstatus[10:8],mstatus[3],mstatus[6:4],1'b0,      mstatus[2:0]}
                                                                           : {mstatus[`XLEN-1:8]                     ,1'b1,      mstatus[6:4],mstatus[7],mstatus[2:0]} :      //此处暂未正确实现
                                                                             wr_data;
@@ -103,7 +103,7 @@ always @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
         mcause <= `XLEN'h0;
     end
-    else if((sel_mcause | trap) && stall_n) begin
+    else if((sel_mcause | trap |in_intr_ls) && stall_n) begin
         mcause <= (trap || in_intr_ls) ? mcause_n:      //此处暂未正确实现  //已实现ecall
                         wr_data;
     end
