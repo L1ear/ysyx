9d252fddd6b966f22517ef0ac17bf5d3c2a1bf73
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 01:47:24 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:47:24 up 15:26,  1 user,  load average: 0.21, 0.46, 0.46

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -5,7 +5,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   // printf("%x\n",*(((uint32_t *)(c)+2*32+1)));
-  if (user_handler) {
+  if (!user_handler) {
     Event ev = {0};
     
     switch (c->mcause) {
