12446db67cb638669131b8211422c02449a7aa48
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-24 00:22:15 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:22:15 up 17:41,  1 user,  load average: 1.53, 1.20, 1.18

--- npc/csrc_pip/timer.c
+++ npc/csrc_pip/timer.c
@@ -4,8 +4,8 @@ static uint64_t boot_time = 0;
 
 static uint64_t get_time_internal() {
   struct timespec now;
-  clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
+  gettimeofday(&now, NULL);
+  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
   return us;
 }
 
