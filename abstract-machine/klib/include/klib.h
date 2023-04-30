069b0acbb3aa393072bdf004211d15f6adecc2b7
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-30 21:23:52 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:23:52 up  6:01,  1 user,  load average: 0.73, 0.61, 0.58

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -13,11 +13,9 @@ extern "C" {
 
 // string.h
 void  *memset    (void *s, int c, size_t n);
-void  *mymemset    (void *s, int c, size_t n);
 void  *memcpy    (void *dst, const void *src, size_t n);
 void  *memmove   (void *dst, const void *src, size_t n);
 int    memcmp    (const void *s1, const void *s2, size_t n);
-int    mymemcmp    (const void *s1, const void *s2, size_t n);
 size_t strlen    (const char *s);
 char  *strcat    (char *dst, const char *src);
 char  *strcpy    (char *dst, const char *src);
