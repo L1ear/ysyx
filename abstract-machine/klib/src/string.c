9617f4c49e9afc7d0f72433531948d7ee2f6d040
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-30 21:18:58 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:18:58 up  5:56,  1 user,  load average: 1.03, 0.78, 0.62

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -114,7 +114,7 @@ The  memset()  function  fills  the  first  n  bytes of the memory area
 pointed to by s with the constant byte c.
 The memset() function returns a pointer to the memory area s.
 */
-void *memset(void *s, int c, size_t n) {
+void *mymemset(void *s, int c, size_t n) {
   // panic("Not implemented");
   asm volatile (
     "li a6, 0 \n\t"
@@ -131,7 +131,15 @@ void *memset(void *s, int c, size_t n) {
   );
   return s;
 }
-
+void *memset(void *s, int c, size_t n) {
+  // panic("Not implemented");
+  size_t i = 0;
+  for(i = 0; i < n;i ++)
+  {
+    ((char*)s)[i] = c; 
+  }
+  return s;
+}
 /*
 The  memmove()  function  copies n bytes from memory area src to memory
 area dest.  The memory areas may overlap: copying takes place as though
