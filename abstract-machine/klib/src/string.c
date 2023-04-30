2a03f934d2e43709ed6ad47550f582219d97f45d
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-30 20:42:13 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:42:13 up  5:20,  1 user,  load average: 0.46, 0.37, 0.32

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -106,6 +106,7 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 /*
 The  memset()  function  fills  the  first  n  bytes of the memory area
 pointed to by s with the constant byte c.
+The memset() function returns a pointer to the memory area s.
 */
 void *memset(void *s, int c, size_t n) {
   // panic("Not implemented");
