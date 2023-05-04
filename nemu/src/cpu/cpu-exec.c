287de465dd3fc8d9c41d36bc8845ac547ebafaac
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-04 21:30:33 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:30:33 up 23:03,  1 user,  load average: 8.36, 7.93, 7.37

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -59,8 +59,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   // int i;
-  char *inst = (char *)&s->isa.inst.val;
-  printf("!!!!!!!!!!!!!!\n%s\n",inst);
+  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
+  printf("!!!!!!!!!!!!!!\n%s\n",(char *)inst);
   // for (i = ilen-1; i >= 0; i --) {
     p += snprintf(p, strlen(inst), " %s", inst);
   // }
