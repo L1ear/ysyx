d617a97aaf8a1144a350749b35060faa3d59e5f3
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-17 22:40:43 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:40:43 up 1 day, 24 min,  1 user,  load average: 0.47, 0.71, 0.67

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,13 +6,13 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = W;  // TODO: get the correct width
-  int h = H;  // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
-  // while(1);
+  // int i;
+  // int w = W;  // TODO: get the correct width
+  // int h = H;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
+  // // while(1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -24,6 +24,17 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *framebuffer = (uint32_t *)FB_ADDR;
+  uint16_t ps_x = ctl->x, ps_y = ctl->y;
+  uint32_t *pixelbuffer = (uint32_t *)ctl->pixels;
+  uint16_t px_w = ctl->w, px_h = ctl->h;
+  int x,y;
+  for(y=0; y<px_h; y++){
+    for(x=0; x<px_w; x++){
+      framebuffer[(ps_y+y)*W + (ps_x + x)] = *(pixelbuffer + y*px_w +x);
+    }
+  }
+
   // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   // uint32_t *pixels = (uint32_t*)(ctl->pixels);
   // int x = ctl->x, y = ctl->y;
