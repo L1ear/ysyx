117ae10539731db19a765dd422801f814a081a37
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-17 22:42:31 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:42:31 up 1 day, 26 min,  1 user,  load average: 1.06, 0.79, 0.70

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,14 +1,14 @@
 #include <am.h>
 #include <nemu.h>
 
-# define W    400
-# define H    300
+# define frame_W    400
+# define frame_H    300
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
   // int i;
-  // int w = W;  // TODO: get the correct width
-  // int h = H;  // TODO: get the correct height
+  // int w = frame_W;  // TODO: get the correct width
+  // int h = frame_H;  // TODO: get the correct height
   // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   // for (i = 0; i < w * h; i ++) fb[i] = i;
   // outl(SYNC_ADDR, 1);
@@ -18,7 +18,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = W, .height = H,
+    .width = frame_W, .height = frame_H,
     .vmemsz = 0
   };
 }
@@ -31,19 +31,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x,y;
   for(y=0; y<px_h; y++){
     for(x=0; x<px_w; x++){
-      framebuffer[(ps_y+y)*W + (ps_x + x)] = *(pixelbuffer + y*px_w +x);
+      framebuffer[(ps_y+y)*frame_W + (ps_x + x)] = *(pixelbuffer + y*px_w +x);
     }
   }
-
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // uint32_t *pixels = (uint32_t*)(ctl->pixels);
-  // int x = ctl->x, y = ctl->y;
-  // int w = ctl->w, h = ctl->h;
-  // for(int j = 0; j< h; j++){
-  //   for(int i = 0; i< w; i++){
-  //     fb[(y+j)*W+(x+i)] = *(pixels+j*w+i);
-  //   }
-  // }
+  //写完一帧后刷新
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
