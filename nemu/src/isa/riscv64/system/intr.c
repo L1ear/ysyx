e042478b20da4fdf84e442736321c992b6c44ef0
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-14 23:39:33 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-72-generic #79~20.04.1-Ubuntu SMP Thu Apr 20 22:12:07 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:39:33 up  8:17,  1 user,  load average: 0.57, 0.25, 0.25

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -6,8 +6,6 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   cpu.mepc = epc;
   cpu.mcause = NO;
-  //{mstatus[`XLEN-1:13],2'b11,mstatus[10:8],mstatus[3],mstatus[6:4],1'b0,mstatus[2:0]}
-  cpu.mcause = BITS(cpu.mcause,63,13)<<13|0b00<<11|BITS(cpu.mcause,10,8)<<8|BITS(cpu.mcause,3,3)<<7|BITS(cpu.mcause,6,4)<<4|BITS(cpu.mcause,2,0);
   // Log("ecall has been called,mcause: %016lx\n",NO);
   return cpu.mtvec;
 }
