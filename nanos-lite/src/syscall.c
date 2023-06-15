5c4ce583b2c7d20925cc4139444a08770d6c637a
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-15 21:24:49 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-72-generic #79~20.04.1-Ubuntu SMP Thu Apr 20 22:12:07 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:24:49 up 20:23,  1 user,  load average: 0.78, 0.74, 0.52

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,6 +3,8 @@
 #include <sys/time.h>
 #include <proc.h>
 
+#define strace
+
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
@@ -23,17 +25,48 @@ void do_syscall(Context *c) {
     // void *buf = (void*) a[2];
     // size_t len = a[3];
   switch (a[0]) {
-    case SYS_yield: yield(); break;
-    case SYS_exit:  halt(a[1]);break;
-
-    case SYS_brk:  c->GPRx = 0; break;
-    case SYS_open: c->GPRx = fs_open((void *)a[1], a[2], a[3]); break;
-    case SYS_read: c->GPRx = fs_read(a[1], (void*)a[2], a[3]); break;
-    case SYS_lseek:c->GPRx = fs_lseek(a[1], (long)a[2], a[3]); break;
-    case SYS_write:c->GPRx = fs_write(a[1], (const void *)a[2], a[3]); break;
-    case SYS_close:c->GPRx = fs_close(a[1]); break;
-    case SYS_gettimeofday: c->GPRx = sys_gettimeofday((struct timeval *)a[1],(struct timezone *)a[2]); break;
-    case SYS_execve: c->GPRx = sys_execve((char*)a[1],(char**)a[2],(char**)a[3]); break;
+    case SYS_yield: 
+    #ifdef strace
+      printf("syscall yield!\n");
+    #endif
+      yield(); 
+      break;
+    case SYS_exit:  
+    #ifdef strace
+      printf("syscall halt: %d\n",a[1]);
+    #endif
+      halt(a[1]);
+      break;
+    case SYS_brk:  
+    #ifdef strace
+      printf("syscall brk\n");
+    #endif
+      c->GPRx = 0; 
+      break;
+    case SYS_open: 
+      c->GPRx = fs_open((void *)a[1], a[2], a[3]); 
+    #ifdef strace
+      printf("syscall openat:%s, flag:%d, mode:%d return %d\n",(void *)a[1], a[2], a[3],c->GPRx);
+    #endif
+      break;
+    case SYS_read: 
+      c->GPRx = fs_read(a[1], (void*)a[2], a[3]); 
+      break;
+    case SYS_lseek:
+      c->GPRx = fs_lseek(a[1], (long)a[2], a[3]); 
+      break;
+    case SYS_write:
+      c->GPRx = fs_write(a[1], (const void *)a[2], a[3]); 
+      break;
+    case SYS_close:
+      c->GPRx = fs_close(a[1]); 
+      break;
+    case SYS_gettimeofday: 
+      c->GPRx = sys_gettimeofday((struct timeval *)a[1],(struct timezone *)a[2]); 
+      break;
+    case SYS_execve: 
+      c->GPRx = sys_execve((char*)a[1],(char**)a[2],(char**)a[3]); 
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
