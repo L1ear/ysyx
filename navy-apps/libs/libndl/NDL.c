5445e526c1779b7a48d88f6f039d9736957c5929
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-18 02:30:02 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-72-generic #79~20.04.1-Ubuntu SMP Thu Apr 20 22:12:07 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:30:02 up 1 day, 11:37,  1 user,  load average: 1.09, 1.12, 1.02

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -34,15 +34,15 @@ int NDL_PollEvent(char *buf, int len) {
   //printf("%d\n",len);
   int ret = read(fb_event,buf,len);
   if(ret == 0) return 0;
-  // return ret;
-  for(int i = 0; i < len;i++)
-  {
-    if(buf[i] == '\n') 
-    {
-      buf[i] = '\0';
-      return ret;
-    }
-  }
+  return ret;
+  // for(int i = 0; i < len;i++)
+  // {
+  //   if(buf[i] == '\n') 
+  //   {
+  //     buf[i] = '\0';
+  //     return ret;
+  //   }
+  // }
   /* if(buf[0]=='0') {
     //printf("NDL NULL\n");
     return 0;
