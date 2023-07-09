c71283b5ce2c7e3ab96321473b709c7f696d57c3
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-09 23:42:14 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-75-generic #82~20.04.1-Ubuntu SMP Wed Jun 7 19:37:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:42:14 up 21:24,  1 user,  load average: 1.15, 1.06, 1.09

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -137,9 +137,9 @@ static inline fixedpt fixedpt_divi(fixedpt A, int B) {
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-	if((fixedpt)(((fixedpt)A*(fixedpt)B)>>(fixedpt)8)!=(fixedpt)(((fixedptd)A*(fixedptd)B)/(fixedptd)256)))
-		printf("Error!\n%x:%x\n",(fixedpt)(((fixedpt)A*(fixedpt)B)/(fixedpt)256),(fixedpt)(((fixedptd)A*(fixedptd)B)/(fixedptd)256)));
-	return (fixedpt)(((fixedpt)A*(fixedpt)B)>>(fixedpt)8);
+	if((fixedpt)(((fixedptd)A*(fixedptd)B)>>8)!=(fixedpt)(((fixedptd)A*(fixedptd)B)/(fixedptd)256)))
+		printf("Error!\n%x:%x\n",(fixedpt)(((fixedptd)A*(fixedptd)B)>>8),(fixedpt)(((fixedptd)A*(fixedptd)B)/(fixedptd)256)));
+	return (fixedpt)(((fixedptd)A*(fixedptd)B)>>8);
 }
 
 
