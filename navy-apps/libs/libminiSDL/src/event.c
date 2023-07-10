1993fe1e7a488a7350766b5332fd79ddc2eae7c7
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-10 23:05:38 +0800

    >  compile NEMU ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-75-generic #82~20.04.1-Ubuntu SMP Wed Jun 7 19:37:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:05:38 up 1 day,  4:24,  1 user,  load average: 1.71, 1.07, 0.72

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -12,57 +12,13 @@ static const char *keyname[] = {
 
 
 static uint8_t keystate[83]={0};
-
 int SDL_PushEvent(SDL_Event *ev) {
-  printf("please implement me\n");
-  assert(0);
   return 0;
 }
 
-
 int SDL_PollEvent(SDL_Event *ev) {
-  //printf("in\n");
-  char key[64]={0};
-  key[0] = '\0';
-  int keycode = 0;
-  int ret = NDL_PollEvent(key,sizeof(key));
-  //printf("SDL %s\n",key);
-  if(key[0] == '\0') {
-    ev->key.keysym.sym = SDLK_NONE;
-    ev->type = SDL_USEREVENT;
-    return 0;
-  }
-  else {
-    char* str = key+3;
-    //printf("%s %d\n",key,strlen(key));
-    for(int i = 0,cnt = 0;i < sizeof(keyname);i+=strlen(keyname[cnt]),cnt++)
-    {
-      //printf("start %d %s %s %d %d %d end\n",cnt,keyname[cnt],str,strlen(str),!strncmp(keyname[cnt],str,strlen(str)),i);
-      if(!strncmp(keyname[cnt],str,strlen(str))&&!strncmp(keyname[cnt],str,strlen(keyname[cnt]))) {
-        keycode = cnt;
-        break;
-      }
-    }
-    //printf("in waitevent %d\n",keycode);
-    if(key[1] == 'u')
-    {
-      ev->key.keysym.sym = keycode;
-      ev->type = SDL_KEYUP;
-      keystate[keycode] = 0;
-    }
-    else if(key[1] == 'd'){
-      ev->key.keysym.sym = keycode;
-      ev->type = SDL_KEYDOWN;
-      keystate[keycode] = 1;
-    }
-    else assert(0);
-  //printf("please implement me\n");
-  //assert(0);
-    return 1;
-  }
-  
+  return 0;
 }
-
 
 int SDL_WaitEvent(SDL_Event *event) {
   char key[64]={0};
@@ -97,17 +53,9 @@ int SDL_WaitEvent(SDL_Event *event) {
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  printf("please implement me\n");
-  assert(0);
   return 0;
 }
 
-
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  if(numkeys == NULL)
-    return keystate;
-  else assert(0);
-  //printf("please implement me\n");
-  //assert(0);
-  //else assert(0);
+  return NULL;
 }
