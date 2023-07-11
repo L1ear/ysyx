ce0c475c43447ea770d893e7141fdbe597b41fd0
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-12 01:27:53 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-75-generic #82~20.04.1-Ubuntu SMP Wed Jun 7 19:37:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:27:53 up 1 day, 19:39,  1 user,  load average: 0.94, 1.30, 1.35

--- npc/soc/lsu.v
+++ npc/soc/lsu.v
@@ -1,60 +1,38 @@
 
 module lsu (
-    input                           clk,rstn,
     input                           wren,rden,
     input           [2      :0]     memop,    
     input           [`XLEN-1:0]     wr_data_i,
-    input           [`XLEN-1:0]     addr_i,
-    input           [`XLEN-1:0]     pc_ls_i,
-    input           [`XLEN-1:0]     addr_last_i,
-    input                           wren_last_i,
+    input           [32-1:0]     addr_i,
 
     output          [`XLEN-1:0]     ls_res_o,
     output                          ls_not_ok,    
 
 //sram interface
-    output          [`XLEN-1:0]     ls_sram_addr,
+    // output          [`XLEN-1:0]     ls_sram_addr,
     output                          ls_sram_rd_en,              //这里不再需要addr_valid信号了，而是有两个en信号代替，一个时钟周期内要么读要么写，不会有歧义
     output                          ls_sram_wr_en,
     output          [`XLEN-1:0]     ls_sram_wr_data,
     output          [7      :0]     ls_sram_wr_mask,
     output          [2      :0]     ls_sram_wr_size,
     output          [2      :0]     ls_sram_rd_size,
-    input                           ls_sram_rd_data_valid,
-    input                           ls_sram_wr_data_ok,
+    // input                           ls_sram_rd_data_valid,
+    input                           ls_sram_data_not_ok,
     input           [`XLEN-1:0]     ls_sram_rd_data
 );
 
 
 
 wire    [`XLEN-1:0]     rd_data_base;
-// wire    [`XLEN-1:0]     dpi_addr = addr_i & ~`XLEN'h7;
-// always @(negedge clk) begin                     //这里使用了下降沿，是为了避免verilator的时序问题，在加入cache后应修改
-//     if(rden)
-//         vmemread(addr_i, 8, rd_data_base, pc_ls_i);
-// end
-// reg     ls_ok;
-// always @(posedge clk or negedge rstn) begin
-//     if(~rstn) begin
-//         ls_ok <= 1'b0;
-//     end 
-//     else begin
-//         if((rden & ls_sram_rd_data_valid) || (wren & ls_sram_wr_data_ok)) begin
-//             ls_ok <= 1'b1;
-//         end
-//         else begin
-//             ls_ok <= 1'b0;
-//         end
-//     end
-// end
 
-assign  ls_sram_addr = addr_i;
+
+// assign  ls_sram_addr = addr_i;
 assign  ls_sram_rd_en = rden;// && ~ls_ok;
 assign  ls_sram_wr_en = wren;// && ~ls_ok;
 assign  ls_sram_wr_mask = wr_mask;
 assign  ls_sram_wr_data = wr_data_i;
 assign  rd_data_base = ls_sram_rd_data;
-assign  ls_not_ok = ls_sram_wr_data_ok;
+assign  ls_not_ok = ls_sram_data_not_ok;
 assign  ls_sram_wr_size = wr_size;
 assign  ls_sram_rd_size = rd_size;
 
@@ -154,56 +132,6 @@ assign  ls_res_o = `XLEN'b0
 //写mem-------------------------------------------------------------------
 
 
-// wire    [`XLEN-1:0]     wr_mask;  
-// wire                    sb,sh,sw,sd;  
-
-// assign  sb = wren & (memop == `sb);
-// assign  sh = wren & (memop == `sh);
-// assign  sw = wren & (memop == `sw);
-// assign  sd = wren & (memop == `sd);
-
-
-// reg     [`XLEN-1:0]     wr_data_b;
-// reg     [`XLEN-1:0]     wr_data_h;
-// reg     [`XLEN-1:0]     wr_data_w;
-// always@(*)begin
-// 		case(addr_i[2:0])
-// 			3'b000:wr_data_b={rd_data_base[63:8],wr_data_i[7:0]};
-// 			3'b001:wr_data_b={rd_data_base[63:16],wr_data_i[7:0],rd_data_base[7:0]};
-// 			3'b010:wr_data_b={rd_data_base[63:24],wr_data_i[7:0],rd_data_base[15:0]};
-// 			3'b011:wr_data_b={rd_data_base[63:32],wr_data_i[7:0],rd_data_base[23:0]};
-//             3'b100:wr_data_b={rd_data_base[63:40],wr_data_i[7:0],rd_data_base[31:0]};
-// 			3'b101:wr_data_b={rd_data_base[63:48],wr_data_i[7:0],rd_data_base[39:0]};
-// 			3'b110:wr_data_b={rd_data_base[63:56],wr_data_i[7:0],rd_data_base[47:0]};
-// 			3'b111:wr_data_b={wr_data_i[7:0],rd_data_base[55:0]};
-// 		endcase
-//         case(addr_i[2:1])
-//             2'b00:wr_data_h={rd_data_base[63:16],wr_data_i[15:0]};
-// 			2'b01:wr_data_h={rd_data_base[63:32],wr_data_i[15:0],rd_data_base[15:0]};
-// 			2'b10:wr_data_h={rd_data_base[63:48],wr_data_i[15:0],rd_data_base[31:0]};
-// 			2'b11:wr_data_h={wr_data_i[15:0],rd_data_base[47:0]};
-//         endcase
-//         case(addr_i[2])
-//             1'b0:wr_data_w={rd_data_base[63:32],wr_data_i[31:0]};
-// 			1'b1:wr_data_w={wr_data_i[31:0],rd_data_base[31:0]};
-//         endcase
-// end
-
-// wire    [`XLEN-1:0] wr_data;
-// assign  wr_data = `XLEN'b0
-//                   |({`XLEN{sb}} & (wr_data_b))
-//                   |({`XLEN{sh}} & (wr_data_h))
-//                   |({`XLEN{sw}} & (wr_data_w))
-//                   |({`XLEN{sd}} & (wr_data_i));
-
-
-// reg     [`XLEN-1:0] wr_data_buf;
-// always @(posedge clk) begin
-//     if(wren) begin
-//         vmemwrite(dpi_addr, wr_data, pc_ls_i);
-//         wr_data_buf <= wr_data;
-//     end
-// end
 reg     [2:0]     wr_size;  //给cache的话，其实只要两位，mask可不要
 reg     [7:0]     wr_mask;  
 wire                    sb,sh,sw,sd;  
---
+++ npc/vsrc_pip/EX/muldiv/mul64.v
@@ -0,0 +1,103 @@
+module mul64 (
+    input           clk,rst_n,
+    
+    input           mul_valid,
+    input           flush,
+    input           mulw,
+    input   [1:0]   mul_signed,
+    input   [63:0]  multiplicand,
+    input   [63:0]  multiplier,
+
+    output          mul_ready,
+    output          out_valid,
+    output  [63:0]  result_hi,
+    output  [63:0]  result_lo
+);
+//mul_signed[1:0]:    
+//2'b11(signed x signed);
+//2'b10(signed x unsigned);
+//2'b00(unsigned x unsigned);
+
+
+
+wire               src1_is_signed;
+wire               src2_is_signed;
+wire [32:0]        mul_lo_src1;
+wire [32:0]        mul_hi_src1;
+wire [32:0]        mul_lo_src2;
+wire [32:0]        mul_hi_src2;
+wire [65:0]        mul_ll_rslt;//mul_lo_src1 x mul_lo_src2
+wire [65:0]        mul_lh_rslt;//mul_lo_src1 x mul_hi_src2
+wire [65:0]        mul_hl_rslt;//mul_hi_src1 x mul_lo_src2
+wire [65:0]        mul_hh_rslt;//mul_hi_src1 x mul_hi_src2
+
+wire [65:0]        mul_ll_rslt_ex2;//mul_lo_src1 x mul_lo_src2
+wire [65:0]        mul_lh_rslt_ex2;//mul_lo_src1 x mul_hi_src2
+wire [65:0]        mul_hl_rslt_ex2;//mul_hi_src1 x mul_lo_src2
+wire [65:0]        mul_hh_rslt_ex2;//mul_hi_src1 x mul_hi_src2
+
+wire[129:0]                        mul_ll_rslt_ext_ex2;
+wire[129:0]                        mul_lh_rslt_ext_ex2;
+wire[129:0]                        mul_hl_rslt_ext_ex2;
+wire[129:0]                        mul_hh_rslt_ext_ex2;
+wire[129:0]                        mul_rslt_stg2_ex2;
+
+wire                               mul_stg1_vld_set;
+wire                               mul_stg1_vld_clr;
+wire                               mul_stg1_vld_d;
+reg                                mul_stg1_vld_q;
+wire                               int_reg_wr_ready_fin;
+
+//1st stage
+assign src1_is_signed   = mul_signed[1];
+assign src2_is_signed   = mul_signed[0];
+
+
+assign mul_lo_src1      = {1'b0,multiplicand[31:0]};
+assign mul_hi_src1      = {src1_is_signed & multiplicand[63],multiplicand[63:32]};
+assign mul_lo_src2      = {1'b0,multiplier[31:0]};
+assign mul_hi_src2      = {src2_is_signed & multiplier[63],multiplier[63:32]};
+
+assign mul_ll_rslt      = mul_lo_src1 * mul_lo_src2;
+assign mul_lh_rslt      = $signed(mul_lo_src1) * $signed(mul_hi_src2);
+assign mul_hl_rslt      = $signed(mul_hi_src1) * $signed(mul_lo_src2);
+assign mul_hh_rslt      = $signed(mul_hi_src1) * $signed(mul_hi_src2);
+
+assign mul_ready        = ~mul_stg1_vld_q | int_reg_wr_ready_fin;
+assign mul_stg1_vld_set = mul_valid & mul_ready & ~flush;
+assign mul_stg1_vld_clr = int_reg_wr_ready_fin | flush;
+assign mul_stg1_vld_d   = mul_stg1_vld_set | mul_stg1_vld_q & ~mul_stg1_vld_clr & ~flush;
+
+always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+        mul_stg1_vld_q <= 'b0;
+    end
+    else
+        mul_stg1_vld_q <= mul_stg1_vld_d;
+end
+stl_reg #(
+  .WIDTH     (66*4),
+  .RESET_VAL (0)
+)id_reg(
+  .i_clk   (clk),
+  .i_rst_n (rst_n),
+  .i_wen   (mul_stg1_vld_set),
+  .i_din   ({mul_ll_rslt, mul_lh_rslt, mul_hl_rslt, mul_hh_rslt}),
+  .o_dout  ({mul_ll_rslt_ex2, mul_lh_rslt_ex2, mul_hl_rslt_ex2, mul_hh_rslt_ex2})
+);
+
+assign mul_ll_rslt_ext_ex2  = {{130-66{1'b0}},mul_ll_rslt_ex2};
+assign mul_lh_rslt_ext_ex2  = {{32{mul_lh_rslt_ex2[65]}},mul_lh_rslt_ex2,32'h0};
+assign mul_hl_rslt_ext_ex2  = {{32{mul_hl_rslt_ex2[65]}},mul_hl_rslt_ex2,32'h0};
+assign mul_hh_rslt_ext_ex2  = {mul_hh_rslt_ex2,64'h0};
+
+assign int_reg_wr_ready_fin = mul_stg1_vld_q;
+
+assign mul_rslt_stg2_ex2    = $signed(mul_ll_rslt_ext_ex2) + $signed(mul_lh_rslt_ext_ex2) + $signed(mul_hl_rslt_ext_ex2) + $signed(mul_hh_rslt_ext_ex2);
+
+assign result_hi = mul_rslt_stg2_ex2[127:64];
+assign result_lo = mulw ? {{32{mul_rslt_stg2_ex2[31]}}, mul_rslt_stg2_ex2[31:0]} : mul_rslt_stg2_ex2[63:0];
+
+assign out_valid = mul_stg1_vld_q;
+endmodule
+
