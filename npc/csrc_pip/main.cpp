b39210c606fae9fea7c717ba43c70406f3cdb378
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-11 22:37:43 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-75-generic #82~20.04.1-Ubuntu SMP Wed Jun 7 19:37:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:37:43 up 1 day, 16:49,  1 user,  load average: 1.43, 1.27, 0.90

--- npc/csrc_pip/main.cpp
+++ npc/csrc_pip/main.cpp
@@ -316,7 +316,7 @@ void single_cycle(int i) {
           //               写串口的指令
           // printf("%04d   %04d   %d\n",instr_last&(uint8_t)0x7f,(uint8_t)0x23,(instr_last&(uint8_t)0x7f)==((uint8_t)0x23));
           if((((instr_last & (uint8_t)0x7f) == ((uint8_t)0x23)) ||((instr_last & (uint8_t)0x7f) == ((uint8_t)0x03))) ){    //跳过printf和读取时间
-            printf("!!!!!!!!!!!!!!!!!reg:%d,value:%08x",(instr_last & (uint8_t)0xf8000)>>14,cpu.gpr[(instr_last & (uint8_t)0xf8000)>>14]);
+            printf("reg:%d,value:%08x\n",(instr_last & (uint8_t)0xf8000)>>14,cpu.gpr[(instr_last & (uint8_t)0xf8000)>>14]);
             if((cpu.gpr[(instr_last & (uint8_t)0xf8000)>>14])>>31){
               difftest_skip_ref();
             }
