6c7a4a74dd1408f52a4726e1297b297cfc04df26
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-19 13:35:12 +0800

    >  sim RTL ysyx_22040734 Liu jiajun Linux ubuntu 5.15.0-83-generic #92~20.04.1-Ubuntu SMP Mon Aug 21 14:00:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:35:12 up 40 min,  1 user,  load average: 0.42, 0.66, 0.51

--- npc/csrc_pip/timer.c
+++ npc/csrc_pip/timer.c
@@ -11,8 +11,8 @@ static uint64_t get_time_internal() {
 
 uint64_t get_time() {
   if (boot_time == 0) boot_time = get_time_internal();
-  // uint64_t now = boot_time + nr_cycle>>(uint8_t)6;
-  uint64_t now = get_time_internal();
+  uint64_t now = boot_time + nr_cycle>>(uint8_t)6;
+  // uint64_t now = get_time_internal();
   // if(nr_cycle>>6 >= 1000000)
   // printf("+1ms\n");
   return now - boot_time;
